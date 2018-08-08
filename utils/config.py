Mellanox/mkt

commit 5cfd525f9307a6ec68f15a5fb19922ced6fa9429

Rename 'x' to 'mkt'
This seems like a better name for a utility.

Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

utils/config.py
@@ -6,7 +6,7 @@ import configparser
 import pwd
 import grp
 
-config_f = os.path.expanduser('~') + '/.x-tools/' + socket.gethostname() + '.xt';
+config_f = os.path.expanduser('~') + '/.config/mellanox/mkt/' + socket.gethostname() + '.mkt';
 config = configparser.ConfigParser(allow_no_value=True);
 
 username = pwd.getpwuid( os.getuid() )[ 0 ];
@@ -21,7 +21,8 @@ def load():
     try:
         return config["defaults"];
     except KeyError:
-        exit(socket.gethostname() + ' machine doesn\'t exist in ~/.x-tools/.\nPlease run setup before. Exiting ...');
+        exit(("%r machine doesn\'t exist in %r.\nPlease run setup before. Exiting ...")%
+             (socket.gethostname(), config_f));
 
 def init():
     config.read(config_f);

utils/cmdline.py
@@ -50,9 +50,13 @@ def load_all_commands(name,top_module):
         yield (k,fn,argsfn);
 
 def main(cmd_modules,top_module):
-    parser = argparse.ArgumentParser(description='x-tools helper commands')
-    subparsers = parser.add_subparsers(title="These are common commands " +
-                                "used in various situations", dest="command");
+    parser = argparse.ArgumentParser(
+        description="""Mellanox Kernel Toolset
+
+Various utilities for working with the Linux kernel at Mellanox""");
+    subparsers = parser.add_subparsers(
+        title="Sub commands",
+        dest="command");
     subparsers.required = True;
 
     commands = []

commit bd612c51183802fcb92731821a7346b5f6986fbc

Rename configuration to be according to README
The suggested configuration file is named hv-hostname.mkt,
so this file should be looked after.

Fixes: 5cfd525f9307 ("Rename 'x' to 'mkt'")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

utils/config.py
@@ -7,7 +7,7 @@ import pwd
 import grp
 
 config_f = os.path.expanduser(
-    '~') + '/.config/mellanox/mkt/' + socket.gethostname() + '.mkt'
+    '~') + '/.config/mellanox/mkt/hv-' + socket.gethostname() + '.mkt'
 config = configparser.ConfigParser(allow_no_value=True)
 
 username = pwd.getpwuid(os.getuid())[0]