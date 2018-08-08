Mellanox/mkt

commit 3dbab2f387a1a83c8ac16daeed8c42dbc4d57b53

Revise how images works and change the names of the docker images
- Use harbor.mellanox.com to tag the final image. This will allow that
  image to push.pull to the registry
- Use local_mtk for the internal image steps
- Get rid of the 'container' mode
- Use docker build pull in the proper places
- Use argparse properly
- Tidy run a bit so it doesn't blow up immediately for me

Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

plugins/cmd_run.py
@@ -191,22 +191,6 @@ def cmd_vfio_enable(args):
         switch_to_vfio(I, modalias)
 
 
-def native_run(args):
-    cont = docker_get_containers(name=args.os)
-    if cont:
-        try:
-            docker_call(["kill", *cont])
-        except subprocess.CalledProcessError:
-            pass
-        docker_call(["rm", *cont])
-
-    docker_exec([
-        "run", "--rm", "--net=host", "--privileged",
-        "--name=%s" % (args.os), "--tty", "--interactive",
-        "lab/native:%s" % (args.os)
-    ])
-
-
 def get_mac():
     mac = None
     try:
@@ -253,18 +237,23 @@ def get_pickle(args):
 
     return base64.b64encode(pickle.dumps(p)).decode()
 
+from . import cmd_images
 
 def args_run(parser):
+    section = load()
     parser.add_argument(
-        "--os", nargs=1, choices=sorted([
-            "fc28",
-        ]), help="The OS to run")
+        "os",
+        nargs=1,
+        help="The OS image name to run",
+        choices=sorted(cmd_images.supported_os),
+        default=section.get('os', 'fc28'))
     parser.add_argument(
         "image",
         nargs='?',
         choices=sorted(get_images()),
-        help="The image to run")
-    parser.add_argument('--kernel', help="Path to the kernel tree to boot")
+        help="The IB card configuration to use")
+    parser.add_argument('--kernel', help="Path to the kernel tree to boot",
+                        default=section.get('linux',None))
     parser.add_argument(
         '--dir', action="append", help="Other paths to map", default=[])
     parser.add_argument(
@@ -286,26 +275,14 @@ def args_run(parser):
         default=[],
         choices=sorted(get_pci_rdma_devices().keys()),
         help="Pass a given PCI bus/device/function to the guest")
-    parser.add_argument(
-        '--native',
-        action="store_true",
-        default=False,
-        help="Run a native container, without kvm inside")
 
 
 def cmd_run(args):
     """Run a system image container inside KVM"""
     check_not_root()
     section = load()
+    args.os = args.os[0];
 
-    if not args.os:
-        args.os = section['os']
-        if not args.os:
-            args.os = "fc28"
-
-    if args.native:
-        native_run(args)
-        exit()
     """
     We have three possible options to execute:
     1. "x run" without request to specific image. We will try to find default one
@@ -318,10 +295,10 @@ def cmd_run(args):
     s = set()
     if not args.pci and not args.simx:
         if not args.image:
-            args.image = section['image']
+            args.image = section.get('image', None)
 
     if args.image:
-        pci = get_images(args.image)['pci']
+        pci = get_imagesq(args.image)['pci']
         s = pci.split()
 
     union = set(get_simx_rdma_devices()).union(
@@ -337,7 +314,7 @@ def cmd_run(args):
     args.simx += set(s).intersection(set(get_simx_rdma_devices()))
 
     if not args.kernel:
-        args.kernel = section['linux']
+        exit("Must specify a linux kernel with --kernel, or a config file default")
 
     # Invoke ourself as root to manipulate sysfs
     if args.pci:
@@ -380,5 +357,5 @@ def cmd_run(args):
         "%s:/plugins:ro" % (src_dir), "--rm", "--net=host", "--privileged",
         "--name=%s" % (args.os), "--tty", "-e",
         "KVM_PICKLE=%s" % (get_pickle(args)), "--interactive",
-        "lab/kvm:%s" % (args.os)
+        make_image_name("kvm", args.os)
     ] + do_kvm_args)

docker/fc28/Dockerfile
@@ -1,7 +1,0 @@
-FROM lab/base:fc28
-
-RUN /root/basic-setup.sh && /root/container-setup.sh
-
-STOPSIGNAL 37
-EXPOSE 22
-CMD ["/bin/bash","/root/do-init.sh","--unit","container.target"]

docker/fc28/base.Dockerfile
@@ -47,4 +47,4 @@ RUN \
 
 ADD sshd_config ssh_host_rsa_key /etc/ssh/
 
-ADD basic-setup.sh container-setup.sh kvm-setup.sh do-init.sh /root/
+ADD basic-setup.sh kvm-setup.sh /root/

docker/fc28/container-setup.sh
@@ -1,23 +1,0 @@
-#!/bin/bash
-set -e
-
-# sshd on an alternate port
-cat <<EOF > /etc/systemd/system/sshd.socket
-[Socket]
-ListenStream=23
-Accept=yes
-
-[Install]
-WantedBy=sockets.target
-EOF
-systemctl enable sshd.socket
-
-# Special target for booting everything needed for the container mode
-cat <<EOF > /etc/systemd/system/container.target
-[Unit]
-Description=Containerized system
-Requires=basic.target remote-fs.target systemd-user-sessions.service shell-console.service
-Conflicts=rescue.service rescue.target
-After=basic.target remote-fs.target systemd-user-sessions.service rescue.service rescue.target
-AllowIsolate=yes
-EOF

docker/fc28/do-init.sh
@@ -1,13 +1,0 @@
-#!/bin/bash
-
-# Unprivileged access to RDMA devices
-mkdir -p /dev/infiniband
-chmod g+rw -R /dev/infiniband/
-chgrp 1000 -R /dev/infiniband/
-
-# Switching /sys to ro informs systemd it is running in a container and that
-# it should disable certain operations, like starting udev.
-export container=docker
-mount -o remount,ro /sys
-
-exec /sbin/init $*

docker/fc28/kvm.Dockerfile
@@ -1,6 +1,6 @@
-FROM lab/simx:fc28
+FROM local_mkt/kvm_simx:fc28
 
 # Get the built simx from the simx builder container
-COPY --from=lab/simx:fc28 /opt/simx /opt/simx
+COPY --from=local_mkt/kvm_simx:fc28 /opt/simx /opt/simx
 
 RUN /root/basic-setup.sh && /root/kvm-setup.sh

docker/fc28/simx.Dockerfile
@@ -1,4 +1,4 @@
-FROM lab/base:fc28
+FROM local_mkt/kvm_base:fc28
 
 RUN dnf install -y \
     python \

plugins/cmd_images.py
@@ -7,6 +7,10 @@ from utils.docker import *
 from subprocess import call, Popen
 from utils.cmdline import *
 
+supported_os = {
+    "fc28",
+}
+
 
 def args_images(parser):
     parser.add_argument(
@@ -16,10 +20,13 @@ def args_images(parser):
         help="Do not update the base image",
         default=True)
 
+    section = utils.config.load()
     parser.add_argument(
-        "os", nargs='?', help="The image to build", choices=sorted([
-            "fc28",
-        ]))
+        "os",
+        nargs='?',
+        help="The image to build",
+        choices=sorted(supported_os),
+        default=section.get('os', 'fc28'))
     """
     parser.add_argument("arch",
                         nargs='?',
@@ -52,28 +59,28 @@ def cmd_images(args):
     """Build docker image for different architectures and OS."""
     check_not_root()
 
-    section = utils.config.load()
-    if not args.os:
-        args.os = section['os']
-        if not args.os:
-            args.os = "fc28"
-
     with in_directory(
             os.path.join(os.path.dirname(__file__), "../docker/", args.os)):
-        cmd = ["build"] + get_proxy_arg() + (["--pull"] if args.pull else
-                                             ["--pull=false"])
+        # Host network is needed because docker has a hard time in some cases
+        # finding the right DNS server for Mellanox's private DNS. (eg over
+        # VPN)
+        cmd = ["build", "--network=host"] + get_proxy_arg()
 
-        docker_call(cmd + [
-            "-t",
-            make_image_name("lab/base", args.os), "-f", "base.Dockerfile", "."
-        ])
-
-        docker_call(cmd + [
-            "-t",
-            make_image_name("lab/simx", args.os), "-f", "simx.Dockerfile", "."
-        ])
-
+        # Pull can only be done for this image as the other FROMs in the later
+        # images refer to internal images. Docker does not handle this well.
         docker_call(cmd + (["--pull"] if args.pull else ["--pull=false"]) + [
             "-t",
-            make_image_name("lab/kvm", args.os), "-f", "kvm.Dockerfile", "."
+            make_local_image_name("kvm_base", args.os), "-f",
+            "base.Dockerfile", "."
+        ])
+
+        docker_call(cmd + [
+            "-t",
+            make_local_image_name("kvm_simx", args.os), "-f",
+            "simx.Dockerfile", "."
+        ])
+
+        docker_call(cmd + [
+            "-t",
+            make_image_name("kvm", args.os), "-f", "kvm.Dockerfile", "."
         ])

commit b8bf9b0ead3c8dfbbf0e3960e177b003b7aea23f

Fix type in call to get_iamges()
Fixes: 3dbab2f387a1 ("Revise how images works and change the names of the docker images")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/cmd_run.py
@@ -298,7 +298,7 @@ def cmd_run(args):
             args.image = section.get('image', None)
 
     if args.image:
-        pci = get_imagesq(args.image)['pci']
+        pci = get_images(args.image)['pci']
         s = pci.split()
 
     union = set(get_simx_rdma_devices()).union(