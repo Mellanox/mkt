Mellanox/mkt

commit dfb5e4b63a09a3e2530cdcb1d04d5ffe8cdf53a2

Move VFIO functions to separate file
Latest image changes caused to the situation where we called to mkt
files with wrong user because of sudo.

_  mkt git:(setups) mkt run
'nps-server-14' machine doesn't exist in
'/root/.config/mellanox/mkt/hv-nps-server-14.mkt'.
Please run setup before. Exiting ...
Traceback (most recent call last):
  File "/labhome/leonro/src/mkt//mkt", line 22, in <module>
    utils.cmdline.main(cmd_modules, plugins)
  File "/.autodirect/swgwork/leonro/src/mkt/utils/cmdline.py", line 84,
in main
    args.func(args)
  File "/.autodirect/swgwork/leonro/src/mkt/plugins/cmd_run.py", line
330, in cmd_run
    ["--pci=%s" % (I) for I in args.pci])
  File "/usr/lib64/python3.6/subprocess.py", line 291, in check_call
    raise CalledProcessError(retcode, cmd)
subprocess.CalledProcessError: Command '['sudo',
'/labhome/leonro/src/mkt//mkt', 'vfio-enable', '--pci=0000:05:00.0',
'--pci=0000:88:00.0']'
 returned non-zero exit status 1.

As an outcome of this change, the "mkt -h" won't present internal
vfio-enable command tot eh users.

Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

utils/vfio.py
@@ -1,0 +1,60 @@
+# Authors:
+#   Leon Romanovsky <leonro@mellanox.com>
+#   Jason Gunthorpe <jgg@mellanox.com>
+import os
+import sys
+import re
+import argparse
+
+def switch_to_vfio(bdf, modalias):
+    """Switch the kernel driver for a PCI device to vfio-pci so it can be used
+    with VFIO passthrough."""
+    cur_driver = os.path.join("/sys/bus/pci/devices", bdf, "driver")
+    if os.path.exists(cur_driver):
+        if os.readlink(cur_driver).endswith("vfio-pci"):
+            return
+
+    if not os.path.exists("/sys/bus/pci/drivers/vfio-pci"):
+        subprocess.check_call(["modprobe", "vfio-pci"])
+
+    # There kernel does not de-dup this list and provides no wy for us to
+    # see, it so, remove then add.
+    with open(
+            "/sys/bus/pci/drivers/vfio-pci/remove_id", "wb", buffering=0) as F:
+        val = "%s %s\n" % (modalias['v'], modalias['d'])
+        try:
+            F.write(val.encode())
+            F.flush()
+        except IOError:
+            # can get enodev if there is no ID in the list
+            pass
+    with open("/sys/bus/pci/drivers/vfio-pci/new_id", "w") as F:
+        F.write("%s %s\n" % (modalias['v'], modalias['d']))
+
+    if os.path.exists(cur_driver):
+        with open(os.path.join(cur_driver, "unbind"), "w") as F:
+            F.write(bdf + "\n")
+    with open("/sys/bus/pci/drivers/vfio-pci/bind", "w") as F:
+        F.write(bdf + "\n")
+
+    assert os.readlink(cur_driver).endswith("vfio-pci")
+
+
+def vfio_enable(args):
+    """Move the given PCI BDF to the vfio driver. This is an internal command used
+    automatically by kvm-run"""
+    sd = "/sys/bus/pci/devices/"
+    for I in args.pci:
+        with open(os.path.join(sd, I, "modalias")) as F:
+            modalias = F.read().strip()
+        modalias = {
+            a: b
+            for a, b in re.findall(r"([a-z]+)([0-9A-F]+)", modalias)
+        }
+        switch_to_vfio(I, modalias)
+
+parser = argparse.ArgumentParser(description="VFIO enable")
+parser.add_argument('--pci', metavar="PCI_BDF", action="append",
+                    default=[], help="PCI BDF to move to vfio")
+args = parser.parse_args()
+vfio_enable(args)

plugins/cmd_run.py
@@ -12,7 +12,6 @@ from utils.docker import *
 from utils.cmdline import *
 from utils.config import *
 
-
 class DirList(object):
     def __init__(self):
         self.list = set()
@@ -129,67 +128,6 @@ def get_pci_rdma_devices():
                 }
                 devices[I] = modalias
     return devices
-
-
-# -------------------------------------------------------------------------
-
-
-def switch_to_vfio(bdf, modalias):
-    """Switch the kernel driver for a PCI device to vfio-pci so it can be used
-    with VFIO passthrough."""
-    cur_driver = os.path.join("/sys/bus/pci/devices", bdf, "driver")
-    if os.path.exists(cur_driver):
-        if os.readlink(cur_driver).endswith("vfio-pci"):
-            return
-
-    if not os.path.exists("/sys/bus/pci/drivers/vfio-pci"):
-        subprocess.check_call(["modprobe", "vfio-pci"])
-
-    # There kernel does not de-dup this list and provides no wy for us to
-    # see, it so, remove then add.
-    with open(
-            "/sys/bus/pci/drivers/vfio-pci/remove_id", "wb", buffering=0) as F:
-        val = "%s %s\n" % (modalias['v'], modalias['d'])
-        try:
-            F.write(val.encode())
-            F.flush()
-        except IOError:
-            # can get enodev if there is no ID in the list
-            pass
-    with open("/sys/bus/pci/drivers/vfio-pci/new_id", "w") as F:
-        F.write("%s %s\n" % (modalias['v'], modalias['d']))
-
-    if os.path.exists(cur_driver):
-        with open(os.path.join(cur_driver, "unbind"), "w") as F:
-            F.write(bdf + "\n")
-    with open("/sys/bus/pci/drivers/vfio-pci/bind", "w") as F:
-        F.write(bdf + "\n")
-
-    assert os.readlink(cur_driver).endswith("vfio-pci")
-
-
-def args_vfio_enable(parser):
-    parser.add_argument(
-        '--pci',
-        metavar="PCI_BDF",
-        action="append",
-        default=[],
-        help="PCI BDF to move to vfio")
-
-
-def cmd_vfio_enable(args):
-    """Move the given PCI BDF to the vfio driver. This is an internal command used
-    automatically by kvm-run"""
-    sd = "/sys/bus/pci/devices/"
-    for I in args.pci:
-        with open(os.path.join(sd, I, "modalias")) as F:
-            modalias = F.read().strip()
-        modalias = {
-            a: b
-            for a, b in re.findall(r"([a-z]+)([0-9A-F]+)", modalias)
-        }
-        switch_to_vfio(I, modalias)
-
 
 def get_mac():
     mac = None
@@ -322,8 +260,9 @@ def cmd_run(args):
 
     # Invoke ourself as root to manipulate sysfs
     if args.pci:
-        subprocess.check_call(["sudo", sys.argv[0], "vfio-enable"] +
-                              ["--pci=%s" % (I) for I in args.pci])
+        subprocess.check_call(["sudo", sys.executable,
+                               os.path.join(os.path.dirname(__file__), "../utils/vfio.py")] +
+                               ["--pci=%s" % (I) for I in args.pci])
 
     cont = docker_get_containers(name=docker_os)
     if cont:

commit c49a9af600320e634ca3a7c03c6e9bc90f63bcb1

vfio: Add forgotten subprocess module import
Fixes: dfb5e4b63a09 ("Move VFIO functions to separate file")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

utils/vfio.py
@@ -5,6 +5,7 @@ import os
 import sys
 import re
 import argparse
+import subprocess
 
 def switch_to_vfio(bdf, modalias):
     """Switch the kernel driver for a PCI device to vfio-pci so it can be used