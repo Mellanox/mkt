Mellanox/mkt

commit f0b6bf5010a43ddcf9b5d905b84648f85c9e2420

images: Rework how the 'support' images are built
Instead of describing simx with a docker file that does git fetch/etc
handle the git fetch externally in the mkt script and inject a source tar
directly into docker. This allows us to fully cache the git fetches which
are very slow over VPN.

This is preparation for adding a bunch more built-from-source support
modules.

Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

utils/cmdline.py
@@ -12,12 +12,23 @@ import shlex
 
 source_root = os.path.normpath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+cache_dir = None
 
 
 def get_internal_fn(fn):
     """Return the full path to an internal file. When running "in-place" this path
     points into the source directory"""
     return os.path.join(source_root, fn)
+
+
+def get_cache_fn(fn):
+    """Return a 'cache' filename. Cache files are ones that can be deleted without
+    an impact on the operation of mkt - at worst it will run slower."""
+    global cache_dir
+    if cache_dir is None:
+        cache_dir = os.path.expanduser("~/.cache/mellanox/mkt/")
+        os.makedirs(cache_dir, exist_ok=True)
+    return os.path.join(cache_dir, fn)
 
 
 def query_yes_no(question, default="yes"):
@@ -84,9 +95,11 @@ def my_print_help(cmd_fn_name, fallback_fn, file=None):
         "pandoc -s -t man %s | man -l -" % (shlex.quote(pd_fn))
     ])
 
+
 def check_not_root():
     if not os.getuid():
         exit("Please don't run this program as root")
+
 
 def main(cmd_modules, top_module):
     parser = argparse.ArgumentParser(description="""Mellanox Kernel Toolset

docker/fc28/kvm.Dockerfile
@@ -45,8 +45,7 @@ RUN \
     rdma-core \
     && dnf clean dbcache packages
 
-# Get the built simx from the simx builder container
-COPY --from=local_mkt/kvm_simx:fc28 /opt/simx /opt/simx
+COPY --from=local_mkt/support_simx:fc28 /opt/simx /opt/simx
 
 ADD sshd_config ssh_host_rsa_key /etc/ssh/
 

docker/fc28/support-simx.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+# ---
+# git_url: http://webdev01.mtl.labs.mlnx:8080/git/simx.git
+# git_commit: 0bb8c6753fd8314071bce3fd70a49f5b51803fa1
+# other_files:
+#   - 0001-Fix-compilation-error-on-FC28.patch
 
-set -e
-
-cd /opt/src/simx
-
-git reset --hard 0bb8c6753fd8314071bce3fd70a49f5b51803fa1
-git config --global user.email "leonro@mellanox.com"
-git config --global user.name "Leon Romanovsky"
-git am /root/00*.patch
+patch -p1 < /opt/00*.patch
 
 # See ./mlnx_infra/config.status.mlnx
 SIMX_GCC_FLAGS=
@@ -15,9 +13,10 @@ SIMX_EXTRA_FLAGS="-I/usr/include/libnl3/"
 SIMX_EXTRA_LDFLAGS=
 SIMX_EXTRA_COMPILATION_FLAGS='--enable-werror'
 
-make clean
+mkdir build
+cd build
 
-'./configure' \
+'../configure' \
 ${SIMX_GCC_FLAGS} \
 ${SIMX_EXTRA_COMPILATION_FLAGS} \
 '--python=/usr/bin/python2' \
@@ -69,6 +68,8 @@ ${SIMX_EXTRA_COMPILATION_FLAGS} \
 
 make -j`nproc`
 make install
-rm -rf /opt/src
 mkdir -p /opt/simx/etc/qemu-kvm/
 ln -s /etc/qemu/bridge.conf /opt/simx/etc/qemu-kvm/bridge.conf
+
+cd ..
+rm -rf build

docker/fc28/support.Dockerfile
@@ -14,6 +14,7 @@ RUN dnf install -y \
     libseccomp-devel \
     libusb-devel \
     make \
+    patch \
     pixman-devel \
     pkg-config \
     pulseaudio \
@@ -24,11 +25,4 @@ RUN dnf install -y \
     usbredir-devel \
     uuid-devel \
     zlib-devel \
-    && dnf clean all
-
-ADD do-simx.sh 00*.patch /root/
-
-RUN mkdir -p /opt/src/ && \
-    cd /opt/src/ && \
-    http_proxy= git clone http://webdev01.mtl.labs.mlnx:8080/git/simx.git && \
-    /bin/bash /root/do-simx.sh
+    && dnf clean dbcache packages

plugins/cmd_images.py
@@ -2,48 +2,15 @@
 """
 import os
 import utils
+import yaml
 from utils.git import *
 from utils.docker import *
+from utils.cmdline import *
 from subprocess import call, Popen
 
 supported_os = {
     "fc28",
 }
-
-def args_images(parser):
-    parser.add_argument(
-        "--no-pull",
-        dest="pull",
-        action="store_false",
-        help=
-        "Do not update the base docker images from the public docker registry",
-        default=True)
-    parser.add_argument(
-        "--push",
-        action="store_true",
-        help=
-        "Upload created images to docker registry (need to have an account in harbor registry)",
-        default=False)
-
-    section = utils.load_config_file()
-    parser.add_argument(
-        "os",
-        nargs='?',
-        help="The image to build",
-        choices=sorted(supported_os),
-        default=section.get('os', 'fc28'))
-    """
-    parser.add_argument("arch",
-                        nargs='?',
-                        help="Architecture to build",
-                        choices=sorted([
-                            "x86_64",
-                            "arm64",
-                            "ppc64",
-                            "ppc64le",
-                        ]),
-                        default="x86_64");
-    """
 
 
 def get_proxy_arg(run=False):
@@ -75,21 +42,258 @@ def do_pull(dockerfn):
     return images
 
 
+class SupportImage(object):
+    """Support images are docker images that exist only to compile from source for
+    another container. We continue to use docker images for this purpose as
+    docker provides a stable and reproducible environment for the build and a
+    caching system to make development run faster.
+
+    Images produces this way should have their output included in the file
+    image via a COPY --from directive"""
+    cdir = None
+
+    def __init__(self, osname, script):
+        if SupportImage.cdir is None:
+            SupportImage.cdir = get_cache_fn("")
+
+        self.name = re.match(r"support-(.*)\.sh", script).group(1)
+        self.script = script
+        self.docker_tag = make_local_image_name("support_%s" % (self.name),
+                                                osname)
+
+        # The script starts with a yaml meta-data header in bash comments
+        with open(script, "rb") as F:
+            ln = iter(F)
+            text = []
+            for I in ln:
+                if I == b"# ---\n":
+                    text.append(I[2:])
+                    break
+            else:
+                raise ValueError("Bad script %r, missing header" % (script))
+            for I in ln:
+                if not I.startswith(b"# "):
+                    break
+                text.append(I[2:])
+            else:
+                raise ValueError("Bad script %r, mising trailer" % (script))
+        self.spec = yaml.load(b"".join(text))
+
+    def _fetch_git(self):
+        """Make sure that the required commit ID is available under ~/.cache/ for
+        later use, fetching the git repo if necessary"""
+
+        git_url = self.spec["git_url"]
+        git_ref = self.spec["git_commit"]
+
+        # FIXME: We could look in the config file for an existing local git
+        # repo for the project..
+
+        self.git_dir = os.path.join(SupportImage.cdir,
+                                    os.path.basename(git_url))
+        if not self.git_dir.endswith(".git"):
+            self.git_dir = self.git_dir + ".git"
+        if not os.path.exists(self.git_dir):
+            try:
+                git_call(["clone", "--bare", git_url, self.git_dir])
+            except subprocess.CalledProcessError:
+                shutil.rmtree(self.git_dir)
+
+        with in_directory(self.git_dir):
+            self.git_id = git_commit_id(git_ref, fail_is_none=True)
+            if self.git_id is not None:
+                return
+
+            git_call(["fetch", "--tags", git_url])
+            self.git_id = git_commit_id(git_ref)
+
+    def _fetch_nfs(self):
+        """This is very annoying but we need some files from NFS. If the system is on
+        the mellanox network then just copy them from NFS, otherwise sftp them over and
+        cache them."""
+        self.nfs_paths = {}
+        for nfs_fn, inf in self.spec.get("nfs_files", {}).items():
+            if os.path.exists(nfs_fn):
+                self.nfs_paths[nfs_fn] = nfs_fn
+                continue
+
+            cache_fn = "".join(
+                c if re.match(r'\w', c) else '_' for c in nfs_fn)
+            cache_fn = os.path.join(SupportImage.cdir, cache_fn)
+            self.nfs_paths[nfs_fn] = cache_fn
+            if os.path.exists(cache_fn):
+                continue
+
+            # FIXME: This just uses Jason's lab server for lack of something better..
+            tmp_fn = cache_fn + ".tmp"
+            subprocess.check_call(
+                ["sftp",
+                 "gen-l-vrt-197.mtl.labs.mlnx:%s" % (nfs_fn), tmp_fn])
+            os.rename(tmp_fn, cache_fn)
+
+    def fetch(self):
+        """Retrieve any outside materials for this image build"""
+        if "git_commit" in self.spec:
+            self._fetch_git()
+        if "nfs_files" in self.spec:
+            self._fetch_nfs()
+
+    def _make_docker_file(self, dockerfn):
+        """All docker files for building support images start with the same preamble,
+        docker will cache this across the builds."""
+        with open(dockerfn, "wt") as FO:
+            with open("support.Dockerfile", "rt") as FI:
+                for ln in FI:
+                    FO.write(ln)
+            subst = {
+                "tarfn": os.path.basename(self.tarfn),
+                "script": self.script,
+            }
+            for image_tag, inf in self.spec.get("image_files", {}).items():
+                for I in inf["files"]:
+                    FO.write(
+                        "COPY --from=%s %s %s\n" % (image_tag, I, inf["dest"]))
+            for nfs_fn, inf in self.spec.get("nfs_files", {}).items():
+                FO.write(
+                    "ADD %s %s\n" % (os.path.basename(nfs_fn), inf["dest"]))
+            FO.write("ADD {tarfn} /\n".format(**subst))
+            FO.write(
+                "RUN mkdir -p /opt/src && cd /opt/src/ && /bin/bash -e /opt/{script}".
+                format(**subst))
+
+    def _setup_git(self, dfn):
+        with in_directory(self.git_dir):
+            git_call([
+                "archive", "--format=tar", "--prefix", "opt/src/", "-o",
+                self.tarfn, self.git_id
+            ])
+
+    def _setup_nfs(self, dfn):
+        for nfs_fn, inf in self.spec.get("nfs_files", {}).items():
+            shutil.copy(self.nfs_paths[nfs_fn],
+                        os.path.join(dfn, os.path.basename(nfs_fn)))
+
+    def _setup_script(self):
+        """Bundle the support files into the tar file so we only create one docker
+        layer.  This is done with a stable timestamp so that the tar file
+        content is identical if it does not change."""
+        all_files = [self.script]
+        all_files.extend(self.spec.get("other_files", []))
+
+        if os.path.exists(self.tarfn):
+            subprocess.check_call([
+                "tar", "--append", "--mtime=Jan 1 2010", "--file", self.tarfn,
+                "--transform", "s|^|opt/|"
+            ] + all_files)
+        else:
+            subprocess.check_call([
+                "tar", "--create", "--mtime=Jan 1 2010", "--file", self.tarfn,
+                "--transform", "s|^|opt/|"
+            ] + all_files)
+
+    def build_image(self, osname, build_cmd):
+        with tempfile.TemporaryDirectory() as dfn:
+            self.tarfn = self.name + ".tar"
+            self.tarfn = os.path.join(dfn, self.tarfn)
+
+            if "git_commit" in self.spec:
+                self._setup_git(dfn)
+            if "nfs_files" in self.spec:
+                self._setup_nfs(dfn)
+            self._setup_script()
+
+            self._make_docker_file(os.path.join(dfn, "Dockerfile"))
+
+            docker_call(build_cmd + ["-t", self.docker_tag, dfn])
+
+    def get_depends(self):
+        """Return a list of image tags or short names that this image depends on to
+        build."""
+        for image_tag, inf in self.spec.get("image_files", {}).items():
+            yield image_tag
+
+
+def load_supported(args):
+    """Read the support module scripts from the os directory"""
+    supported = {}
+    for fn in os.listdir():
+        if fn.startswith("support-") and fn.endswith(".sh"):
+            img = SupportImage(args.os, fn)
+            supported[img.name] = img
+    imgs = [img for _, img in sorted(supported.items())]
+    for I in list(supported.values()):
+        supported[I.docker_tag] = I
+
+    def add_elm(img):
+        """Topological sorting helper"""
+        if img.name in done:
+            return
+        done.add(img.name)
+        for I in sorted(img.get_depends()):
+            add_elm(supported[I])
+        res.append(img)
+
+    done = set()
+    res = []
+    for I in imgs:
+        add_elm(I)
+    return res
+
+
+def args_images(parser):
+    parser.add_argument(
+        "--no-pull",
+        dest="pull",
+        action="store_false",
+        help=
+        "Do not update the base docker images from the public docker registry",
+        default=True)
+
+    parser.add_argument(
+        "--only",
+        action="store",
+        help="Build only the image with the given docker image tag",
+        default=None)
+
+    parser.add_argument(
+        "--push",
+        action="store_true",
+        help=
+        "Upload created images to docker registry (need to have an account in harbor registry)",
+        default=False)
+
+    section = utils.load_config_file()
+    parser.add_argument(
+        "os",
+        nargs='?',
+        help="The image to build",
+        choices=sorted(supported_os),
+        default=section.get('os', 'fc28'))
+    """
+    parser.add_argument("arch",
+                        nargs='?',
+                        help="Architecture to build",
+                        choices=sorted([
+                            "x86_64",
+                            "arm64",
+                            "ppc64",
+                            "ppc64le",
+                        ]),
+                        default="x86_64");
+    """
+
+
 def cmd_images(args):
-    """Build docker image for different architectures and OS."""
+    """Build docker images for different architectures and OS."""
+
+    if args.push:
+        docker_call(["login", docker_registry_name()])
 
     with in_directory(utils.get_internal_fn(os.path.join("docker", args.os))):
-
-        # Host network is needed because docker has a hard time in some cases
-        # finding the right DNS server for Mellanox's private DNS. (eg over
-        # VPN)
-        cmd = ["build", "--network=host"] + get_proxy_arg()
-
         images = (
-            (make_local_image_name("kvm_simx", args.os), "simx.Dockerfile"),
+            (None, "support.Dockerfile"),
             (make_image_name("kvm", args.os), "kvm.Dockerfile"),
         )
-
         if args.pull:
             to_pull = set()
             for image, dockerfn in images:
@@ -97,12 +301,25 @@ def cmd_images(args):
             for I in to_pull:
                 docker_call(["pull", I])
 
+        support = load_supported(args)
+        for I in support:
+            I.fetch()
+
+        cmd = ["build"] + get_proxy_arg()
+        for I in support:
+            if args.only is not None and not I.docker_tag.startswith(args.only):
+                continue
+
+            print("------- Building support image %s -------" % (I.docker_tag))
+            I.build_image(args.os, cmd)
+
         for image, dockerfn in images:
-            if args.pull:
-                do_pull(dockerfn)
+            if image is None :
+                continue
+
+            if args.only is not None and not image.startswith(args.only):
+                continue
 
             docker_call(cmd + ["-t", image, "-f", dockerfn, "."])
-
-    if args.push:
-        docker_call(["login", docker_registry_name()])
-        docker_call(["push", make_image_name("kvm", args.os)])
+            if args.push:
+                docker_call(["push", image])

commit a22a476e7bb3b88454d02e41ac9dd7bb1192242b

images: Don't cache on labhome
/labhome has low space quote which doesn't allow to build cache
efficiently.

Fixes: f0b6bf5010a4 ("images: Rework how the 'support' images are built")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

utils/cmdline.py
@@ -27,6 +27,10 @@ def get_cache_fn(fn):
     global cache_dir
     if cache_dir is None:
         cache_dir = os.path.expanduser("~/.cache/mellanox/mkt/")
+        # In MTL network, user home directories are located on /labhome
+        # and doesn't have enough space to build cache efficiently.
+        # Do nasty hack and replace labhome with swgwork, not proud, but it works
+        cache_dir = cache_dir.replace('labhome', 'swgwork')
         os.makedirs(cache_dir, exist_ok=True)
     return os.path.join(cache_dir, fn)
 