Mellanox/mkt

commit f43e4d07056ff864087b500919a7a38e092f81ed

images: Rework cache directory discovery
Create symlink to swgwork instead of rewriting path on the fly.

Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

utils/cmdline.py
@@ -29,9 +29,15 @@ def get_cache_fn(fn):
         cache_dir = os.path.expanduser("~/.cache/mellanox/mkt/")
         # In MTL network, user home directories are located on /labhome
         # and doesn't have enough space to build cache efficiently.
-        # Do nasty hack and replace labhome with swgwork, not proud, but it works
-        cache_dir = cache_dir.replace('labhome', 'swgwork')
-        os.makedirs(cache_dir, exist_ok=True)
+        # Do nasty hack and replace labhome with swgwork
+        if not os.path.exists(cache_dir):
+            scache_dir = cache_dir.replace('labhome', 'swgwork')
+            if cache_dir != scache_dir:
+                os.makedirs(scache_dir, exist_ok=True)
+                os.makedirs(os.path.expanduser("~/.cache/mellanox/") , exist_ok=True)
+                subprocess.check_call(["ln", "-s", scache_dir, cache_dir])
+            else:
+                os.makedirs(cache_dir, exist_ok=True)
     return os.path.join(cache_dir, fn)
 
 

commit 32d0fff106f026ee62c54b9655ba26b7958e93ff

images: Remove trailing slash from directory name
The symlink name provided to "ln" command shouldn't include trailing
slash at the end of it.

Fixes: f43e4d07056f ("images: Rework cache directory discovery")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

utils/cmdline.py
@@ -35,7 +35,7 @@ def get_cache_fn(fn):
             if cache_dir != scache_dir:
                 os.makedirs(scache_dir, exist_ok=True)
                 os.makedirs(os.path.expanduser("~/.cache/mellanox/") , exist_ok=True)
-                subprocess.check_call(["ln", "-s", scache_dir, cache_dir])
+                subprocess.check_call(["ln", "-s", scache_dir, cache_dir[:-1]])
             else:
                 os.makedirs(cache_dir, exist_ok=True)
     return os.path.join(cache_dir, fn)