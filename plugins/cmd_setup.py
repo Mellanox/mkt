Mellanox/mkt

commit f5f4cc6a6ad3e6f726647c4b484d38ec1a3758b0

config: Rename kernel code alias from linux to kernel
It was error to call kernel source with "linux" as alias,
update all call sites and documentation to use "kernel"
as an alias for that.

Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/cmd_setup.py
@@ -132,7 +132,7 @@ def cmd_setup(args):
 
             if key == "kernel":
                 shutil.copy(
-                    section['linux'] + "/.config",
+                    section['kernel'] + "/.config",
                     os.path.join(
                         os.path.dirname(__file__), "../configs/kconfig-kvm"))
                 p = subprocess.Popen(["make", "olddefconfig"], cwd=value)

docs/example.mkt
@@ -1,7 +1,7 @@
 [defaults]
 # Paths to source code
 src = /images/leonro/src/
-linux = /images/leonro/src/kernel/
+kernel = /images/leonro/src/kernel/
 rdma-core = /images/leonro/src/rdma-core/
 iproute2 = /images/leonro/src/iproute2/
 # Direcotry to store various output logs

plugins/cmd_run.py
@@ -236,7 +236,7 @@ def args_run(parser):
     kernel.add_argument(
         '--kernel',
         help="Path to the the top of a compiled kernel source tree to boot",
-        default=section.get('linux', None))
+        default=section.get('kernel', None))
     kernel.add_argument(
         '--kernel-rpm', help="Path to a kernel RPM to boot", default=None)
 

utils/config.py
@@ -21,6 +21,11 @@ def group():
 
 try:
     config.read(config_f)
+    # Fix naming error, where we gave to "kernel" folder "linux" as the alias
+    try:
+        config["defaults"]['kernel'] = config["defaults"]['linux']
+    except KeyError:
+        pass
 except configparser.MissingSectionHeaderError:
     exit(config_f + " in wrong format. Exiting ..")
 
@@ -31,7 +36,6 @@ def load_config_file():
     except KeyError:
         return dict()
 
-
 def init_config_file():
     config.read(config_f)
     try:
@@ -39,7 +43,7 @@ def init_config_file():
         # new setup, set defaults
         config["defaults"] = {
             'src': '/images/' + username() + '/src/',
-            'linux': '/images/' + username() + '/src/kernel/',
+            'kernel': '/images/' + username() + '/src/kernel/',
             'rdma-core': '/images/' + username() + '/src/rdma-core/',
             'iproute2': '/images/' + username() + '/src/iproute2/',
             'logs': '/images/' + username() + '/logs/',

commit ae7b234b579a08a1ddf403c6ccfcf5990936bc3c

setup: Remap kernel alias to linux project
In gerrit, the kernel upstream project is named "linux",
rename the search key, so automatic git close will work.

Fixes: f5f4cc6a6ad3 ("config: Rename kernel code alias from linux tockernel")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/cmd_setup.py
@@ -113,6 +113,9 @@ def cmd_setup(args):
             if key == "src" or key == "logs" or key == "ccache":
                 continue
 
+            if key == "kernel":
+                key = "linux"
+
             p = subprocess.Popen(
                 [
                     "git", "clone", "ssh://" + utils.username() +
@@ -130,7 +133,7 @@ def cmd_setup(args):
                 cwd=value)
             p.wait()
 
-            if key == "kernel":
+            if key == "linux":
                 shutil.copy(
                     section['kernel'] + "/.config",
                     os.path.join(