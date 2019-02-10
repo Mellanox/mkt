Mellanox/mkt

commit c2340b973b989295923e1f22d5d9dfc8a029b80f

mkt: Add build options for smart compilation
Provide smart compilation solution to build required
packages (iproute2, rdma-core and kernel) in place from
any place in your path in specific Docker container.

This functionality allows to reduce build requirements
of the hypervisors and allows us to continue and keep
clean HVs without worries of upgrades.

* "mkt build" without any parameters will try to check
  working directory for the known files. If it successes
  to recognize, the mkt will compile working directory
  according to the rules written in scripts/build.*
  file.
* "mkt build iproute2|kernel|rdma-core" will compile code
  in working directory from relevant entry in mkt config.
  This option allows us to execute builder from any place.
* Provide an option to clean source code with "--clean" option.
* Add support to systemd
* Add an option --run-shell to give direct access to the build container.
  The main usage of this option is to invoke kernel menuconfig in that
  container, or perform various build experiments.

Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

docker/fc29/support-rdma_core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ---
-# git_url: https://github.com/linux-rdma/rdma-core.git
-# git_commit: v21
+# git_url: /images/leonro/src/rdma-core
+# git_commit: 988936bf
 
 rpmbuild --build-in-place -bb redhat/rdma-core.spec

docker/fc29/support.Dockerfile
@@ -1,22 +1,30 @@
 FROM fedora:29
 
 RUN dnf install -y \
+    bc \
     binutils \
+    bison \
     cmake \
+    elfutils-devel \
+    elfutils-libelf-devel \
     findutils \
+    flex \
     gcc \
     git-core \
     glib2-devel \
+    hostname \
     libaio-devel \
     libattr-devel \
     libcap-devel \
     libfdt-devel \
+    libmnl-devel \
     libnl3-cli \
     libnl3-devel \
     libseccomp-devel \
     libudev-devel \
     libusb-devel \
     make \
+    ncurses-devel \
     ninja-build \
     pandoc \
     patch \

mkt
@@ -15,6 +15,7 @@ cmd_modules = {
     "cmd_setup",
     "cmd_images",
     "cmd_run",
+    "cmd_build",
 }
 
 if __name__ == "__main__":

plugins/cmd_build.py
@@ -1,0 +1,39 @@
+"""Build sources to remove dependencies from host
+"""
+import os
+import utils
+from utils.build import *
+
+def args_build(parser):
+    parser.add_argument(
+        "project",
+        nargs='?',
+        choices=build_list(),
+        help="Project to build")
+    parser.add_argument(
+        "--clean",
+        action="store_true",
+        help=
+        "Clean source code",
+        default=False)
+    parser.add_argument(
+        '--run-shell',
+        action="store_true",
+        default=False,
+        help="Run a shell inside the container instead of invoking build script")
+
+def cmd_build(args):
+    """Smart build."""
+    from . import cmd_images
+    section = utils.load_config_file()
+    if not args.project:
+        set_args_project(args, section)
+
+    build = BuildSrc(args.project)
+
+    if args.run_shell:
+        do_cmd = ["/bin/bash"]
+    else:
+        do_cmd = build.do_cmd(args.clean)
+
+    docker_exec_run(build.run_cmd(cmd_images.default_os) + do_cmd)

plugins/cmd_images.py
@@ -291,7 +291,7 @@ def cmd_images(args):
 
     with in_directory(utils.get_internal_fn(os.path.join("docker", args.os))):
         images = (
-            (None, "support.Dockerfile"),
+            (make_image_name("build", args.os), "support.Dockerfile"),
             (make_image_name("kvm", args.os), "kvm.Dockerfile"),
         )
         if args.pull:

utils/build.py
@@ -1,0 +1,91 @@
+"""Build sources to remove dependencies from host
+"""
+import os
+import utils
+from utils.docker import *
+
+section = utils.load_config_file()
+
+class Build(object):
+    def  __init__(self, project):
+        self.src = section.get(project, None)
+        self.project = project
+        self.entry = ["bash", "-c"]
+        # Take into acount affinity, but blindly assume
+        # that we can run "2 threads per-CPU".
+        self.num_jobs = len(os.sched_getaffinity(0)) * 2
+
+    def factory(project):
+        if project == "kernel": return KernelBuild(project);
+        if project == "iproute2": return Iproute2Build(project);
+        if project == "rdma-core": return RdmaCoreBuild(project);
+
+    factory = staticmethod(factory)
+    def do_cmd(self, clean=None):
+        if clean:
+            return self._clean()
+        return self._in_place()
+
+    def run_cmd(self, os):
+        ccache = section.get('ccache', None)
+        docker_os = section.get('os', os)
+        return ["--rm", "-v", self.src + ":" + self.src + ":rw",
+                "-it", "-v", ccache + ":/ccache", "-w",
+                self.src, make_image_name("build", docker_os)]
+
+    def rpm(self):
+        """Build RPM from source"""
+        pass
+    def deb(self):
+        """Build DEB from source"""
+        pass
+    def for_install(self):
+        """Compile this source in a way suitable to create runnable container"""
+        pass
+
+class KernelBuild(Build):
+    def _clean(self):
+        return self.entry + ["make clean"]
+    def _in_place(self):
+        print("Start kernel compilation in silent mode")
+        return self.entry + ["make -j%d -s" % (self.num_jobs)]
+
+class Iproute2Build(Build):
+    def _clean(self):
+        return self.entry + ["make distclean"]
+    def _in_place(self):
+        return self.entry + ["make -j%d" % (self.num_jobs)]
+
+class RdmaCoreBuild(Build):
+    def _clean(self):
+        return self.entry + ["rm -rf build"]
+    def _in_place(self):
+        return self.entry + ["./build.sh"]
+
+def BuildSrc(project):
+    for b in Build.__subclasses__():
+        return b.factory(project)
+    raise ValueError
+
+project_marks = {
+        "libibverbs": "rdma-core",
+        "rdma": "iproute2",
+        "kernel": "kernel",
+}
+
+def build_list():
+    return sorted(project_marks.values())
+
+def set_args_project(args, section):
+    """Look in the local folder to determine
+    what we were requested to build"""
+
+    for key, value in project_marks.items():
+        if os.path.isdir(key):
+            args.project = value
+
+    if not args.project:
+        exit("Failed to understand the source of this directory, " +
+             "don't know how to build. Exciting...")
+
+    section[args.project] = os.getcwd()

utils/docker.py
@@ -27,6 +27,14 @@ def docker_exec(args):
         'docker',
     ] + args)
 
+def docker_exec_run(args):
+    """Execute docker docker run with user credentials and display the output to the terminal"""
+    return os.execvp("sudo", [
+        'sudo',
+        'docker',
+        'run',
+        ] + ['--user', "%s:%s" %(os.getuid(), os.getgid())] + args)
+
 
 def docker_output(args, mode=None):
     """Run docker and return the output"""

commit 5dfe3fec7fa3a190a478fe4e51cd39203bdd8d28

docker: Fix overwritten rdma-core git url and update rdma-core version
Fixes: c2340b973b98 ("mkt: Add build options for smart compilation")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

docker/fc29/support-rdma_core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ---
-# git_url: /images/leonro/src/rdma-core
-# git_commit: 988936bf
+# git_url: https://github.com/linux-rdma/rdma-core.git
+# git_commit: v22
 
 rpmbuild --build-in-place -bb redhat/rdma-core.spec