Mellanox/mkt

commit 19eaa5975a025af668393b5a7cdff13ddb176593

build: Configure ccache to speed-up builds
Enable ccache for kernel build for now.

Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

utils/build.py
@@ -33,6 +33,8 @@ class Build(object):
                 "-it", "-v", ccache + ":/ccache"]
         if build_recipe:
             cmd += ["-v", "%s:%s:ro" % (build_recipe, build_recipe)]
+        if ccache:
+            cmd += ["-e", "CCACHE_DIR=/ccache"]
 
         return cmd + ["-w", self.src, make_image_name("build", docker_os)]
 
@@ -51,6 +53,9 @@ class KernelBuild(Build):
         return self.entry + ["make clean"]
     def _in_place(self):
         print("Start kernel compilation in silent mode")
+        ccache = section.get('ccache', None)
+        if ccache:
+            return self.entry + ["make CC=\"ccache gcc\" -j%d -s" % (self.num_jobs)]
         return self.entry + ["make -j%d -s" % (self.num_jobs)]
 
 class Iproute2Build(Build):

docker/fc29/support.Dockerfile
@@ -4,6 +4,7 @@ RUN dnf install -y \
     bc \
     binutils \
     bison \
+    ccache \
     cmake \
     elfutils-devel \
     elfutils-libelf-devel \

commit 59f072db3789139649b551e1d9d16a3c4229faf0

build: Avoid environmental variable in setting ccache directory
We can set directly ccache directory and avoid providing
environmental variables.

Fixes: 19eaa5975a02 ("build: Configure ccache to speed-up builds")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

utils/build.py
@@ -10,7 +10,7 @@ class Build(object):
     def  __init__(self, project):
         self.src = section.get(project, None)
         self.project = project
-        self.entry = ["bash", "-c"]
+        self.entry = ["/bin/bash", "-c"]
         # Take into acount affinity, but blindly assume
         # that we can run "2 threads per-CPU".
         self.num_jobs = len(os.sched_getaffinity(0)) * 2
@@ -29,12 +29,11 @@ class Build(object):
     def run_cmd(self, supos, build_recipe=None):
         ccache = section.get('ccache', None)
         docker_os = section.get('os', supos)
-        cmd = ["--rm", "-v", self.src + ":" + self.src + ":rw",
-                "-it", "-v", ccache + ":/ccache"]
+        cmd = ["--rm", "-v", self.src + ":" + self.src + ":rw", "-it"]
         if build_recipe:
             cmd += ["-v", "%s:%s:ro" % (build_recipe, build_recipe)]
         if ccache:
-            cmd += ["-e", "CCACHE_DIR=/ccache"]
+            cmd += ["-v", ccache + ":/ccache"]
 
         return cmd + ["-w", self.src, make_image_name("build", docker_os)]
 
@@ -55,7 +54,7 @@ class KernelBuild(Build):
         print("Start kernel compilation in silent mode")
         ccache = section.get('ccache', None)
         if ccache:
-            return self.entry + ["make CC=\"ccache gcc\" -j%d -s" % (self.num_jobs)]
+            return self.entry + ["export CCACHE_DIR=/ccache && make CC=\"ccache gcc\" -j%d -s" % (self.num_jobs)]
         return self.entry + ["make -j%d -s" % (self.num_jobs)]
 
 class Iproute2Build(Build):