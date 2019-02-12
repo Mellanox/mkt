9b044304c35f95311e87d50bf096d437c7727875
Leon Romanovsky <leonro@mellanox.com>
2019-02-12 12:37:14 +0200

    image: Install pyverbs by default
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

--- docker/fc29/support-rdma_core.sh
+++ docker/fc29/support-rdma_core.sh
@@ -3,4 +3,4 @@
 # git_url: https://github.com/linux-rdma/rdma-core.git
 # git_commit: v22
 
-rpmbuild --build-in-place -bb redhat/rdma-core.spec
+rpmbuild --build-in-place -bb redhat/rdma-core.spec --with pyverbs
