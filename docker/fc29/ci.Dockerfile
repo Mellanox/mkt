Mellanox/mkt

commit c0f985cedf79bc8197681e6f9e3345c2d7d07d5b

ci: Run kernel checkpatch
mkt ci --no-checkpatch allows to skip this option

Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

docker/fc29/ci.Dockerfile
@@ -1,1 +1,9 @@
 FROM harbor.mellanox.com/mkt/build:fc29
+
+RUN \
+    echo Israel/Jerusalem > /etc/timezone && \
+    echo mellanox.com > /etc/mailname && \
+    dnf install -y \
+    'perl(Term::ANSIColor)' \
+    'perl(Encode)' \
+    && dnf clean dbcache packages

plugins/cmd_ci.py
@@ -10,6 +10,12 @@ def args_ci(parser):
         nargs='?',
         choices=build_list(),
         help="Project to build")
+    parser.add_argument(
+        "--no-checkpatch",
+        action="store_false",
+        dest="checkpatch",
+        help="Skip checkpatch check",
+        default=True)
 
 def cmd_ci(args):
     """Local continuous integration check."""
@@ -19,6 +25,7 @@ def cmd_ci(args):
         set_args_project(args, section)
 
     build = BuildSrc(args.project)
+    build.pickle['checkpatch'] = args.checkpatch
 
     do_cmd = ["python3", "/plugins/do-ci.py"]
     docker_exec(["run"] + build.run_ci_cmd(cmd_images.default_os) + do_cmd)

plugins/do-ci.py
@@ -36,6 +36,11 @@ def fork(args):
         copy("%s/.config" %(args.src), "/build/%s" %(args.project))
 
     args.src = "/build/%s" %(args.project)
+    args.rev = head
+
+def checkpatch(args):
+    subprocess.call(["%s/scripts/checkpatch.pl" %(args.src), "-q", "--no-summary",
+        "-g", args.rev]);
 
 def setup_from_pickle(args, pickle_params):
     """The script that invokes docker passes in some more detailed parameters
@@ -45,6 +50,7 @@ def setup_from_pickle(args, pickle_params):
     args.src = p.get("src", None)
     args.project = p.get("project", None)
     args.rev = p.get("rev", 'HEAD')
+    args.checkpatch = p.get("checkpatch", True)
 
 parser = argparse.ArgumentParser(description='CI container')
 args = parser.parse_args()
@@ -53,5 +59,6 @@ pickle_data = os.environ.get("CI_PICKLE")
 setup_from_pickle(args, pickle_data)
 fork(args)
 
-
-subprocess.check_call(["/bin/bash"])
+if args.project == "kernel":
+    if args.checkpatch:
+        checkpatch(args)

utils/build.py
@@ -17,14 +17,14 @@ class Build(object):
         # Take into acount affinity, but blindly assume
         # that we can run "2 threads per-CPU".
         self.num_jobs = len(os.sched_getaffinity(0)) * 2
+        self.pickle = dict()
 
     def _get_pickle(self):
-        p = dict()
-        p["project"] = self.project
-        p["src"] = self.src
-        p["rev"] = 'HEAD'
+        self.pickle["project"] = self.project
+        self.pickle["src"] = self.src
+        self.pickle["rev"] = 'HEAD'
 
-        return base64.b64encode(pickle.dumps(p)).decode()
+        return base64.b64encode(pickle.dumps(self.pickle)).decode()
 
     def factory(project):
         if project == "kernel": return KernelBuild(project);

commit 025d6b171f3b2fc22aeb9e6a22147f42d95c9ac2

ci: Add missing oython-ply module to fix SPDX check
Traceback (most recent call last):
  File "scripts/spdxcheck.py", line 6, in <module>
    from ply import lex, yacc
ImportError: No module named ply

Fixes: c0f985cedf79 ("ci: Run kernel checkpatch")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

docker/fc29/ci.Dockerfile
@@ -8,6 +8,8 @@ RUN \
     dnf install -y \
     'perl(Term::ANSIColor)' \
     'perl(Encode)' \
+    python-ply \
+    python2-GitPython \
     && dnf clean dbcache packages
 
 RUN rpm -U /opt/rpms/*.rpm