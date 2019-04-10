Mellanox/mkt

commit d113aff09f9166d7dfb8da602197cc1fccf05a60

run: Execute custom boot script
Add new command line option --boot-script which receives path
to custom script. That script will be executed prior to providing
serial console to the user.

The same option is named "boot_script" and can be set in image sections
of configuration file.

Script is needed to be executable (+x bit) and is expected to have
shebang (#!...) line at the beginning.

Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/do-kvm.py
@@ -142,6 +142,22 @@ Where={dfn}
 
         cnt = cnt + 1
 
+def setup_login_script(args):
+    """Create systemd unit to run custom user script right before login"""
+    create_unit(
+        "boot-script", "service", ["getty.target.wants"], """
+[Service]
+User={user}
+Group={group}
+Description=Login script {script}
+ExecStart={script}
+Type=oneshot
+Wants=serial-getty@hvc0.service
+After=serial-getty@hvc0.service
+
+[Install]
+WantedBy=getty.target
+""".format(user=args.user, group=args.group, script=args.boot_script))
 
 def terminal_size():
     import fcntl, termios, struct
@@ -379,6 +395,9 @@ def setup_from_pickle(args, pickle_params):
     args.virt = p.get("virt", False)
     args.vm_addr = VM_Addr(**p["vm_addr"])
     args.mem = p["mem"]
+    args.boot_script = p.get("boot_script", None)
+    args.user = p["user"]
+    args.group = p["group"]
 
 
 parser = argparse.ArgumentParser(
@@ -448,4 +467,8 @@ for k, v in sorted(qemu_args.items()):
         cmd.append(k)
         if v:
             cmd.append(v)
+
+if args.boot_script:
+    setup_login_script(args)
+
 os.execvp(cmd[0], cmd)

docs/example.mkt
@@ -23,6 +23,7 @@ dir = /path/to/first/dir path/to/second/dir /path/to/third/dir
 [cx5-ib]
 # List of PICs to connect to this container
 pci = 0000:05:00.0 0000:88:00.0
+boot_script = /labhome/leonro/ib_bootup_script
 [simx]
 # PCI supports simx-aware names, supported options
 # are: cx4-ib, cx4-eth, cib-ib, cx5-ib, cx5-eth, cx4lx-eth, cx6-ib, cx6-eth

plugins/cmd_run.py
@@ -2,6 +2,7 @@
 """
 import os
 import sys
+import stat
 import pwd
 import grp
 import pickle
@@ -230,8 +231,34 @@ def get_pickle(args, vm_addr):
         p["virt"] = sorted(args.virt)
     p["mem"] = str(mem) + 'G'
 
+    if args.boot_script:
+        p["boot_script"] = args.boot_script
+
     return base64.b64encode(pickle.dumps(p)).decode()
 
+def validate_and_set_boot(args):
+    if not args.boot_script:
+        try:
+            args.boot_script = utils.get_images(args.image)['boot_script']
+        except KeyError:
+            pass
+
+    if not args.boot_script:
+        return None;
+
+    try:
+        executable = stat.S_IXUSR & os.stat(args.boot_script)[stat.ST_MODE]
+    except FileNotFoundError:
+        exit("Wrong path to boot script. Exiting ...")
+
+    if not executable:
+        exit("Bootup script needs to be executable. Exiting ...")
+
+    with open(args.boot_script, 'r') as f:
+        if not f.readline().startswith("#!"):
+            exit("Missing shebang in the first line of boot script. Exiting ...")
+
+    return args.boot_script
 
 def args_run(parser):
     section = utils.load_config_file()
@@ -244,7 +271,7 @@ def args_run(parser):
     kernel = parser.add_mutually_exclusive_group()
     kernel.add_argument(
         '--kernel',
-        help="Path to the the top of a compiled kernel source tree to boot",
+        help="Path to the top of a compiled kernel source tree to boot",
         default=section.get('kernel', None))
     kernel.add_argument(
         '--kernel-rpm', help="Path to a kernel RPM to boot", default=None)
@@ -277,6 +304,10 @@ def args_run(parser):
         default=[],
         choices=sorted(get_virt_rdma_devices()),
         help="Pass a virtual device type-interface format to the guest")
+    parser.add_argument(
+        '--boot-script',
+        help="Path to the custom boot script which will be executed after boot",
+        default=None)
 
 def cmd_run(args):
     """Run a system image container inside KVM"""
@@ -354,6 +385,10 @@ def cmd_run(args):
 
     for I in args.dir:
         mapdirs.add(I)
+
+    args.boot_script = validate_and_set_boot(args)
+    if args.boot_script:
+        mapdirs.add(os.path.dirname(args.boot_script))
 
     vm_addr = get_mac()
 

commit 4f47d48349e3332b6546c6f05a80b74c456b3c94

run: Properly write systemd unit and service file
Fixes: d113aff09f91 ("run: Execute custom boot script")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/do-kvm.py
@@ -149,9 +149,10 @@ def setup_login_script(args):
 [Service]
 User={user}
 Group={group}
-Description=Login script {script}
 ExecStart={script}
 Type=oneshot
+[Unit]
+Description=Login script {script}
 Wants=serial-getty@hvc0.service
 After=serial-getty@hvc0.service
 