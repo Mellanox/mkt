c0886a4d8016d15b5c5e7beb5422f3968827e0ec
Leon Romanovsky <leonro@mellanox.com>
2019-05-02 14:14:39 +0300

    docker: Promote rdma-core to v23
    Use latest released version
    
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

--- docker/fc29/support-rdma_core.sh
+++ docker/fc29/support-rdma_core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ---
 # git_url: https://github.com/linux-rdma/rdma-core.git
-# git_commit: v22
+# git_commit: v23
 
 rpmbuild --build-in-place -bb redhat/rdma-core.spec --with pyverbs
