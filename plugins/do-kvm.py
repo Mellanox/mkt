Mellanox/mkt

commit 119d09ed4525d701b765839c8ed6b99c942256d9

run: Save SimX log file outside of container
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/do-kvm.py
@@ -288,13 +288,13 @@ def set_simx_log():
     # Old SimX version relies on the fact that log file exists,
     # create that file for them
     subprocess.check_call(['mkdir', '-p', '/opt/simx/logs/'])
-    f = open('/opt/simx/logs/simx-qemu.log', 'w+')
+    f = open('/logs/simx-qemu.log', 'w+')
     f.close()
 
     subprocess.check_call(['mkdir', '-p', '/opt/simx/cfg/'])
     with open('/opt/simx/cfg/simx-qemu.cfg', 'a+') as f:
          f.write('[logger]\n')
-         f.write('log_file_redirection = /opt/simx/logs/simx-qemu.log\n')
+         f.write('log_file_redirection = /logs/simx-qemu.log\n')
 
 def set_simx_network(simx):
     """Setup options to start a simx card"""

plugins/cmd_run.py
@@ -445,6 +445,8 @@ def cmd_run(args):
         docker_exec(["run"] + mapdirs.as_docker_bind() + [
             "-v",
             "%s:/plugins:ro" % (src_dir),
+            "--mount",
+            "type=bind,source=%s,destination=/logs" % (utils.config.runtime_logs_dir),
             "--rm",
             "--net=host",
             "--privileged",

utils/__init__.py
@@ -1,5 +1,5 @@
 from .cmdline import get_internal_fn, query_yes_no
-from .config import load_config_file, username, group, init_config_file, get_images
+from .config import load_config_file, username, group, init_config_file, get_images, init_log_dir
 
 __all__ = [
     "get_internal_fn",
@@ -8,5 +8,6 @@ __all__ = [
     "username",
     "group",
     "init_config_file",
+    "init_log_dir",
     "get_images",
 ]

utils/cmdline.py
@@ -9,6 +9,7 @@ import os
 import functools
 import subprocess
 import shlex
+import utils
 
 source_root = os.path.normpath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
@@ -142,4 +143,5 @@ Various utilities for working with the Linux kernel at Mellanox""")
     # argparse will set 'func' to the cmd_* that executes this command
     args = parser.parse_args()
     check_not_root()
+    utils.init_log_dir(args.command)
     args.func(args)

utils/config.py
@@ -70,3 +70,18 @@ def get_images(name=None):
         images.append(I)
 
     return images
+
+runtime_logs_dir = None
+
+def init_log_dir(cmd):
+    from datetime import datetime
+
+    global runtime_logs_dir
+    try:
+        logs = config["defaults"]['logs']
+    except KeyError:
+        logs = "/tmp/"
+
+    d = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+    runtime_logs_dir = logs + cmd + "/" + d + "/"
+    os.makedirs(runtime_logs_dir, exist_ok=True)

commit 3cd70928dbaf8856206673bf785d4dc27f82e165

run: Don't create useless logs folder
There is no need to create logs folder anymore, it is handled
outside of do-kvm.py

Fixes: 119d09ed4525 ("run: Save SimX log file outside of container")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/do-kvm.py
@@ -289,7 +289,6 @@ def set_simx_log():
 
     # Old SimX version relies on the fact that log file exists,
     # create that file for them
-    subprocess.check_call(['mkdir', '-p', '/opt/simx/logs/'])
     f = open('/logs/simx-qemu.log', 'w+')
     f.close()
 