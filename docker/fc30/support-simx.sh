0223f04f058ef37b2289f4b047ce486ae38ae034
Leon Romanovsky <leonro@mellanox.com>
2019-06-19 09:39:21 +0300

    images: Reuse existing SimX configuration script
    Ensure that we build images and local builds in same manner.
    
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

--- docker/fc30/support-simx.sh
+++ docker/fc30/support-simx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ---
 # git_url: http://webdev01.mtl.labs.mlnx:8080/git/simx.git
-# git_commit: v4.1
+# git_commit: dd77dfb88a7f08157819240cb7c99b1f82f57b21
 
 cat <<EOF > mlx-simx.spec
 %global debug_package %{nil}
@@ -16,75 +16,17 @@ License:        Proprietary
 From simx.git
 
 %build
-mkdir build
-cd build
-
-# See ./mlnx_infra/config.status.mlnx
-SIMX_GCC_FLAGS=
-SIMX_EXTRA_FLAGS="-I/usr/include/libnl3/"
-SIMX_EXTRA_LDFLAGS=
-SIMX_EXTRA_COMPILATION_FLAGS='--enable-werror'
-
-'../configure' \
-${SIMX_GCC_FLAGS} \
-${SIMX_EXTRA_COMPILATION_FLAGS} \
-'--python=/usr/bin/python2' \
-'--prefix=/opt/simx' \
-'--libdir=/opt/simx/lib' \
-'--interp-prefix=/opt/simx/qemu-%M' \
-'--with-confsuffix=/qemu-kvm' \
-'--sysconfdir=/opt/simx/etc' \
-'--localstatedir=/opt/simx/var' \
-'--libexecdir=//opt/simx/libexec' \
-"--with-pkgversion=simx-qemu-system-${PKGVERSION}" \
-'--disable-strip' \
-'--disable-qom-cast-debug' \
-"--extra-cflags=-Wundef -Wredundant-decls -Wunreachable-code -Wno-format ${SIMX_EXTRA_FLAGS}" \
-"--extra-ldflags=${SIMX_EXTRA_LDFLAGS}" \
-'--enable-trace-backend=dtrace' \
-'--disable-xen' \
-'--enable-virtfs' \
-'--enable-kvm' \
-'--disable-libusb' \
-'--enable-spice' \
-'--enable-seccomp' \
-'--enable-fdt' \
-'--disable-docs' \
-'--disable-sdl' \
-'--disable-debug-tcg' \
-'--disable-sparse' \
-'--disable-brlapi' \
-'--disable-bluez' \
-'--disable-vde' \
-'--disable-curses' \
-'--disable-curl' \
-'--enable-vnc' \
-'--disable-vnc-sasl' \
-'--enable-linux-aio' \
-'--disable-vnc-png' \
-'--disable-vnc-jpeg' \
-'--disable-gtk' \
-'--disable-vhost-scsi' \
-'--disable-guest-agent' \
-'--disable-glusterfs' \
-'--block-drv-rw-whitelist=qcow,qcow2,raw,file,host_device,nbd,iscsi,vvfat' \
-'--block-drv-ro-whitelist=vmdk,vhdx,vpc' \
-'--disable-rdma' \
-'--disable-tools' \
-'--disable-capstone' \
-'--target-list=x86_64-softmmu'
-
+./mlnx_infra/config.status.mlnx --target=x86 --prefix=/opt/simx
 make %{?_smp_mflags}
 
 #%install
-cd build
 make DESTDIR=%{buildroot} install
-mkdir -p %{buildroot}/opt/simx/etc/qemu-kvm/
-ln -s /etc/qemu/bridge.conf %{buildroot}/opt/simx/etc/qemu-kvm/bridge.conf
+mkdir -p %{buildroot}/etc/qemu-kvm/
+ln -s /etc/qemu/bridge.conf %{buildroot}/etc/qemu-kvm/bridge.conf
 
 %files
 /opt/simx/*
+/etc/qemu-kvm/bridge.conf
 EOF
 
 rpmbuild --build-in-place -bb mlx-simx.spec
-
