Mellanox/mkt

commit 4020378f88dabfbd60d2db7ffb6213bec49cb2d0

run: Give an option to use custom QEMU version
Once you will add "custom_qemu = true" line inside
image section, we will pick QEMU from SimX defined
folder.

Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/cmd_run.py
@@ -243,6 +243,9 @@ def get_pickle(args, vm_addr):
         except KeyError:
             pass
 
+    if args.custom_qemu:
+        p["custom_qemu"] = args.custom_qemu
+
     return base64.b64encode(pickle.dumps(p)).decode()
 
 def validate_and_set_boot(args):
@@ -388,6 +391,21 @@ def cmd_run(args):
             raise ValueError("Kernel path %r is not a directory/does not exist"
                              % (args.kernel))
         mapdirs.add(args.kernel)
+
+    if args.image:
+        try:
+            if utils.get_images(args.image)['custom_qemu'] != "true":
+                raise KeyError
+            args.custom_qemu = section.get('simx', None)
+        except KeyError:
+            args.custom_qemu = None
+
+    if args.custom_qemu:
+        args.custom_qemu = os.path.realpath(args.custom_qemu)
+        if not os.path.isdir(args.custom_qemu):
+            raise ValueError("SimX path %r is not a directory/does not exist"
+                             % (args.custom_qemu))
+        mapdirs.add(args.custom_qemu)
 
     usr = pwd.getpwuid(os.getuid())
     args.dir.append(usr.pw_dir)

plugins/do-kvm.py
@@ -265,6 +265,16 @@ def set_kernel_rpm(src):
         'root=/dev/root rw ignore_loglevel rootfstype=9p rootflags=trans=virtio earlyprintk=serial,ttyS0,115200 console=hvc0'
     })
 
+def set_custom_qemu(tree):
+    """Overwrite installed QEMU variant with local version"""
+    subprocess.check_call(['ln', '-f', '-s',
+        tree + '/x86_64-softmmu/qemu-system-x86_64', '/opt/simx/bin/qemu-system-x86_64'])
+    subprocess.check_call(['ln', '-f', '-s',
+        tree + '/scsi/qemu-pr-helper', '/opt/simx/bin/qemu-pr-helper'])
+    subprocess.check_call(['ln', '-f', '-s',
+        tree + '/fsdev/virtfs-proxy-helper', '/opt/simx/bin/virtfs-proxy-helper'])
+    subprocess.check_call(['ln', '-f', '-s',
+        tree + '/qemu-bridge-helper', '/opt/simx/libexec/qemu-bridge-helper'])
 
 def set_bridge_network(args):
     """If a 'br0' is present then we can setup normal bridge networking"""
@@ -428,6 +438,7 @@ def setup_from_pickle(args, pickle_params):
     args.user = p["user"]
     args.group = p["group"]
     args.num_of_vfs = p.get("num_of_vfs", 0)
+    args.custom_qemu = p.get("custom_qemu", None)
 
 parser = argparse.ArgumentParser(
     description='Launch kvm using the filesystem from the container')
@@ -469,6 +480,9 @@ if args.kernel_rpm:
     set_kernel_rpm(args.kernel_rpm)
 else:
     set_kernel(args.kernel)
+
+if args.custom_qemu:
+    set_custom_qemu(args.custom_qemu)
 
 try:
     subprocess.check_output(

commit 2ffd59c989a3894934ebaec8618d80a1dc4b4176

run: Don't crash if we run without config file
We didn't implement custom QEMU cli command due to low interest, but
forgot to set custom_qemu flag to None. This leaded to python traceback
during manual mkt run execution.

Fixes: 4020378f88da ("run: Give an option to use custom QEMU version")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/cmd_run.py
@@ -399,6 +399,8 @@ def cmd_run(args):
             args.custom_qemu = section.get('simx', None)
         except KeyError:
             args.custom_qemu = None
+    else:
+        args.custom_qemu = None
 
     if args.custom_qemu:
         args.custom_qemu = os.path.realpath(args.custom_qemu)