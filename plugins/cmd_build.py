Mellanox/mkt

commit d600632914bb7d960a46b97f4db1322f7701aaa1

build: Convert from inline commands to general build script
Perform complete rewrite to allow custom builds for user space
applications with kernel headers and rdma-core directly from source/

Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/cmd_build.py
@@ -31,19 +31,22 @@ def cmd_build(args):
     if not args.project:
         set_args_project(args, section)
 
-    build = BuildSrc(args.project)
+    build = Build(args.project)
 
     recipe_dir = None
     if args.build_recipe is not None:
         args.build_recipe = os.path.realpath(args.build_recipe)
         recipe_dir = os.path.dirname(args.build_recipe)
 
-    if args.run_shell:
-        do_cmd = ["/bin/bash"]
-    else:
-        if args.build_recipe:
-            do_cmd = ["/bin/bash", args.build_recipe]
-        else:
-            do_cmd = build.do_cmd(args.clean)
+    build.pickle['shell'] = args.run_shell
+    build.pickle["passwd"] = "%s:x:%s:%s:%s:%s:/bin/bash" % (username(),
+                os.getuid(), os.getgid(), username(), os.getenv("HOME"))
+    build.pickle["group"] = "%s:x:101:" % (group())
+    build.pickle["uid"] = int(os.getuid())
+    build.pickle["gid"] = int(os.getgid())
+    build.pickle["home"] = os.getenv("HOME")
+    build.pickle['clean'] = args.clean
+    build.pickle['build_recipe'] = args.build_recipe
 
-    docker_exec_run(build.run_build_cmd(cmd_images.default_os, recipe_dir) + do_cmd)
+    do_cmd = ["python3", "/plugins/do-build.py"]
+    docker_exec(["run"] + build.run_build_cmd(cmd_images.default_os, recipe_dir) + do_cmd)

plugins/cmd_ci.py
@@ -59,7 +59,7 @@ def cmd_ci(args):
     if not args.project:
         set_args_project(args, section)
 
-    build = BuildSrc(args.project)
+    build = Build(args.project)
     build.pickle['checkpatch'] = args.checkpatch
     build.pickle['sparse'] = args.sparse
     build.pickle['gerrit'] = args.gerrit

plugins/do-build.py
@@ -1,0 +1,77 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import pickle
+import base64
+import argparse
+
+def make_kernel(args):
+    if args.clean:
+        subprocess.check_output(['make', 'clean'])
+        return
+    cmd = ['make']
+    if os.path.isdir('/ccache'):
+        os.environ['CCACHE_DIR'] = '/ccache'
+        cmd += ['CC=ccache gcc']
+
+    print('Start kernel compilation in silent mode')
+    subprocess.check_output(cmd + ['-j%d' %(args.num_jobs), '-s'])
+
+def make_iproute2(args):
+    if args.clean:
+        subprocess.check_output(['make', 'distclean'])
+        return
+    subprocess.check_output(['make', '-j%d' %(args.num_jobs)])
+
+def make_rdma_core(args):
+    if args.clean:
+        subprocess.check_output(['rm', '-rf', 'build'])
+        return
+    subprocess.check_output(['./build.sh'])
+
+def switch_to_user(args):
+    with open("/etc/passwd","a") as F:
+        F.write(args.passwd + "\n");
+    with open("/etc/group","a") as F:
+        F.write(args.group + "\n");
+    os.setgid(args.gid);
+    os.setuid(args.uid);
+    os.environ['HOME'] = args.home
+
+def setup_from_pickle(args, pickle_params):
+    """The script that invokes docker passes in some more detailed parameters
+    about the environment in a pickle and we adjust the configuration
+    accordingly"""
+    p = pickle.loads(base64.b64decode(pickle_params))
+    args.src = p.get("src", None)
+    args.project = p.get("project", None)
+    args.passwd = p.get("passwd", None)
+    args.group = p.get("group", None)
+    args.uid = p.get("uid", None)
+    args.gid = p.get("gid", None)
+    args.clean = p.get("clean", False)
+    args.shell = p.get("shell", False)
+    args.home = p.get('home', None)
+    args.build_recipe = p.get('build_recipe', None)
+
+parser = argparse.ArgumentParser(description='CI container')
+args = parser.parse_args()
+
+args.num_jobs = len(os.sched_getaffinity(0)) * 2
+pickle_data = os.environ.get("BUILD_PICKLE")
+setup_from_pickle(args, pickle_data)
+
+switch_to_user(args)
+if args.shell:
+    os.execvp('/bin/bash', ['/bin/bash'])
+
+if args.build_recipe:
+    os.execvp('/bin/bash', ['/bin/bash', args.build_recipe])
+
+if args.project == "kernel":
+    make_kernel(args)
+if args.project == "iproute2":
+    make_iproute2(args)
+if args.project == "rdma-core":
+    make_rdma_core(args)

utils/build.py
@@ -6,6 +6,7 @@ from utils.docker import *
 import inspect
 import pickle
 import base64
+from utils.config import username, group
 
 section = utils.load_config_file()
 
@@ -13,10 +14,6 @@ class Build(object):
     def  __init__(self, project):
         self.src = section.get(project, None)
         self.project = project
-        self.entry = ["/bin/bash", "-c"]
-        # Take into acount affinity, but blindly assume
-        # that we can run "2 threads per-CPU".
-        self.num_jobs = len(os.sched_getaffinity(0)) * 2
         self.pickle = dict()
 
     def _get_pickle(self):
@@ -24,17 +21,6 @@ class Build(object):
         self.pickle["src"] = self.src
 
         return base64.b64encode(pickle.dumps(self.pickle)).decode()
-
-    def factory(project):
-        if project == "kernel": return KernelBuild(project);
-        if project == "iproute2": return Iproute2Build(project);
-        if project == "rdma-core": return RdmaCoreBuild(project);
-
-    factory = staticmethod(factory)
-    def do_cmd(self, clean=None):
-        if clean:
-            return self._clean()
-        return self._in_place()
 
     def _run_cmd(self, supos, build_recipe, image_name):
         ccache = section.get('ccache', None)
@@ -53,50 +39,16 @@ class Build(object):
         return cmd + ["-w", self.src, make_image_name(image_name, docker_os)]
 
     def run_build_cmd(self, supos, build_recipe=None):
-        return self._run_cmd(supos, build_recipe, "build")
+        cmd = ["-e", "BUILD_PICKLE=%s" % (self._get_pickle())]
+        cmd += ["-v", "%s:%s:ro" %(os.getenv("HOME"), os.getenv("HOME"))]
+
+        return cmd + self._run_cmd(supos, build_recipe, "build")
 
     def run_ci_cmd(self, supos):
         cmd = ["--tmpfs", "/build:rw,exec,nosuid,mode=755,size=10G"]
         cmd += ["-e", "CI_PICKLE=%s" % (self._get_pickle())]
 
         return cmd + self._run_cmd(supos, None, "ci")
-
-    def rpm(self):
-        """Build RPM from source"""
-        pass
-    def deb(self):
-        """Build DEB from source"""
-        pass
-    def for_install(self):
-        """Compile this source in a way suitable to create runnable container"""
-        pass
-
-class KernelBuild(Build):
-    def _clean(self):
-        return self.entry + ["make clean"]
-    def _in_place(self):
-        print("Start kernel compilation in silent mode")
-        ccache = section.get('ccache', None)
-        if ccache:
-            return self.entry + ["export CCACHE_DIR=/ccache && make CC=\"ccache gcc\" -j%d -s" % (self.num_jobs)]
-        return self.entry + ["make -j%d -s" % (self.num_jobs)]
-
-class Iproute2Build(Build):
-    def _clean(self):
-        return self.entry + ["make distclean"]
-    def _in_place(self):
-        return self.entry + ["make -j%d" % (self.num_jobs)]
-
-class RdmaCoreBuild(Build):
-    def _clean(self):
-        return self.entry + ["rm -rf build"]
-    def _in_place(self):
-        return self.entry + ["./build.sh"]
-
-def BuildSrc(project):
-    for b in Build.__subclasses__():
-        return b.factory(project)
-    raise ValueError
 
 project_marks = {
         "libibverbs": "rdma-core",

utils/docker.py
@@ -27,15 +27,6 @@ def docker_exec(args):
         'docker',
     ] + args)
 
-def docker_exec_run(args):
-    """Execute docker docker run with user credentials and display the output to the terminal"""
-    return os.execvp("sudo", [
-        'sudo',
-        'docker',
-        'run',
-        ] + ['--user', "%s:%s" %(os.getuid(), os.getgid())] + args)
-
-
 def docker_output(args, mode=None):
     """Run docker and return the output"""
     o = subprocess.check_output([

commit 48d26639fa0ed9aa29cf81b8b62980e7ce309595

build: Don't hardcode build GID
Build on the unmanaged by Mellanox machine produces the following warning:
➜  kernel git:(rdma-next) mkt build --run-shell
/usr/bin/id: cannot find name for group ID 1000

The reason to it, assumption that user's GID is equal to 101, which is wrong.

Fixes: d600632914bb ("build: Convert from inline commands to general build script")
Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/cmd_build.py
@@ -49,7 +49,7 @@ def cmd_build(args):
     build.pickle['shell'] = args.run_shell
     build.pickle["passwd"] = "%s:x:%s:%s:%s:%s:/bin/bash" % (username(),
                 os.getuid(), os.getgid(), username(), os.getenv("HOME"))
-    build.pickle["group"] = "%s:x:101:" % (group())
+    build.pickle["group"] = "%s:x:%s:" % (group(), os.getgid())
     build.pickle["uid"] = int(os.getuid())
     build.pickle["gid"] = int(os.getgid())
     build.pickle["home"] = os.getenv("HOME")