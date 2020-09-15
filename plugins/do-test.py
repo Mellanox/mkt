cdb825cf24aef906774da66805c8a2af6fccb356
Artem Polyakov <artemp@mellanox.com>
2020-09-15 08:18:06 -0700

    Add url of the test image

--- plugins/do-test.py
+++ plugins/do-test.py
@@ -1,3 +1,5 @@
+# https://people.debian.org/~aurel32/qemu/amd64/debian_wheezy_amd64_standard.qcow2
+
 cmd = ["/opt/simx/bin/qemu-system-x86_64", '-smp', 'cores=2', '-m', '1G', '-nographic',
         '-serial', 'mon:stdio', '-cpu', 'host', '-enable-kvm', '/images/artemp/src/kernel/Debian_check_qemu/debian_wheezy_amd64_standard.qcow2']
 
