2005d6fe4f7aeded2aa52a1539ddf1a5ec140c2e
Artem Y. Polyakov <artpol84@gmail.com>
2020-09-16 15:09:23 -0700

    Fixups

--- plugins/cmd_run.py
+++ plugins/cmd_run.py
@@ -451,6 +451,7 @@ def cmd_run(args):
         do_kvm_args = ["/bin/bash"]
     else:
         do_kvm_args = ["python3", "/plugins/do-kvm.py"]
+#        do_kvm_args = ["python3", "/plugins/do-test.py"]
         if vm_addr.ip:
             # Open network for QEMU, relevant for bridged mode only
             iprule = ["FORWARD", "-m", "physdev", "--physdev-is-bridged", "-j", "ACCEPT"]
@@ -486,7 +487,6 @@ def cmd_run(args):
     else:
         cname = get_container_name(vm_addr)
         print (mapdirs.as_docker_bind())
-#        sys.exit(0)
         docker_exec(["run"] + mapdirs.as_docker_bind() + [
             "-v",
             "%s:/plugins:ro,Z" % (src_dir),
--- plugins/do-test.py
+++ plugins/do-test.py
@@ -1,4 +1,28 @@
-# https://people.debian.org/~aurel32/qemu/amd64/debian_wheezy_amd64_standard.qcow2
+#!/usr/bin/env python3
+# This is intended to be the entry for a docker container that has a bootable OS.
+# It arranges to run the os under KVM with the docker filesystem as the rootfs,
+# and can pass through pci devices to the KVM.
+
+import argparse
+import fnmatch
+import os
+import re
+import shutil
+import socket
+import subprocess
+import collections
+import pickle
+import base64
+import multiprocessing
+import shlex
+import stat
+
+# NOTES:
+#   1. Get image from https://people.debian.org/~aurel32/qemu/amd64/debian_wheezy_amd64_standard.qcow2
+#   2. Update Grub to see kernel images:
+#      - nano /etc/default/grub, add GRUB_CMDLINE_LINUX_DEFAULT="debug earlyprintk=serial console=ttyS0"
+#      - update-grub
+
 
 cmd = ["/opt/simx/bin/qemu-system-x86_64", '-smp', 'cores=2', '-m', '1G', '-nographic',
         '-serial', 'mon:stdio', '-cpu', 'host', '-enable-kvm', '/images/artemp/src/kernel/Debian_check_qemu/debian_wheezy_amd64_standard.qcow2']
