Mellanox/mkt

commit c8b6c3dced27ad972dc9bc5dffc8e5c70e0dbe19

run: Expose internal bind/unbind utility
Move VFIO bind/unbind utility to allow nested call to VFIO module
while we are running it inside other QEMU image.

Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

plugins/cmd_run.py
@@ -407,7 +407,7 @@ def cmd_run(args):
     if args.pci:
         subprocess.check_call([
             "sudo", sys.executable,
-            os.path.join(os.path.dirname(__file__), "../utils/vfio.py")
+            os.path.join(os.path.dirname(__file__), "/vfio.py")
         ] + ["--pci=%s" % (I) for I in args.pci])
 
     mapdirs = DirList()

commit dd41f90b9459c30bf2fe97669bfad53ec540f19d

run: Properly call to bind/unbind utility
The vfio.py script was called with wrong patch and it caused to the
following error when MKT was used with PCI device.

qemu-system-x86_64: -device vfio-pci,host=0000:3b:00.0:
 vfio_pci_vf_token_set: ioctl() failed with err -1

Fixes: c8b6c3dced27 ("run: Expose internal bind/unbind utility")
Reported-by: Shay Drory <shayd@nvidia.com>
Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

plugins/cmd_run.py
@@ -414,7 +414,7 @@ def cmd_run(args):
     if args.pci:
         subprocess.check_call([
             "sudo", sys.executable,
-            os.path.join(os.path.dirname(__file__), "/vfio.py")
+            os.path.join(os.path.dirname(__file__), "vfio.py")
         ] + ["--pci=%s" % (I) for I in args.pci])
 
     mapdirs = DirList()