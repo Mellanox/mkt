Mellanox/mkt

commit e355a573d17a060bab104e93808423df80ad2517

docker: Upgrade all tools and OS
This is mass update to Fedora 34.

Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

docker/fc34/support-rdma_core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ---
 # git_url: https://github.com/linux-rdma/rdma-core.git
-# git_commit: 7f2d460ff23393840088b5ea18df920cea0ee418
+# git_commit: e29a698e99028e9a092bb00c03ee4bfa31ae0cf3
 
 rpmbuild --build-in-place -bb redhat/rdma-core.spec --with pyverbs

docker/fc34/ci.Dockerfile
@@ -1,7 +1,7 @@
-FROM harbor.mellanox.com/mkt/build:fc33
+FROM harbor.mellanox.com/mkt/build:fc34
 
-COPY --from=local_mkt/support_sparse:fc33 /root/rpmbuild/RPMS/x86_64/*.rpm /opt/rpms/
-COPY --from=local_mkt/support_smatch:fc33 /root/rpmbuild/RPMS/x86_64/*.rpm /opt/rpms/
+COPY --from=local_mkt/support_sparse:fc34 /root/rpmbuild/RPMS/x86_64/*.rpm /opt/rpms/
+COPY --from=local_mkt/support_smatch:fc34 /root/rpmbuild/RPMS/x86_64/*.rpm /opt/rpms/
 
 RUN \
     echo Israel/Jerusalem > /etc/timezone && \

docker/fc34/kvm.Dockerfile
@@ -1,7 +1,7 @@
-FROM fedora:33 as rpms
+FROM fedora:34 as rpms
 
-COPY --from=local_mkt/support_rdma_core:fc33 /root/rpmbuild/RPMS/x86_64/*.rpm /opt/rpms/
-COPY --from=local_mkt/support_simx:fc33 /root/rpmbuild/RPMS/x86_64/*.rpm /opt/rpms/
+COPY --from=local_mkt/support_rdma_core:fc34 /root/rpmbuild/RPMS/x86_64/*.rpm /opt/rpms/
+COPY --from=local_mkt/support_simx:fc34 /root/rpmbuild/RPMS/x86_64/*.rpm /opt/rpms/
 
 RUN rm -f \
    /opt/rpms/*debug*.rpm \
@@ -9,7 +9,7 @@ RUN rm -f \
    /opt/rpms/*devel*.rpm \
    /opt/rpms/*iwpmd*.rpm
 
-FROM fedora:33
+FROM fedora:34
 
 # Static files are done before installing to avoid prompting
 ADD ./sudoers /etc/sudoers.d/local

commit c2f86ca588ff03c2628e72049ad32caa603459fd

docker: Fix same declaration of rdma-core version as in base image
Both default rdma-core version that comes with FC34 and declared in
support-rdma_core were v35. This caused to the following error during
image rebuilt:

 ---> 36fcdbcc9b14
Step 9/11 : ADD sshd_config ssh_host_rsa_key /etc/ssh/
 ---> Using cache
 ---> 3dffc7c5da74
Step 10/11 : ADD basic-setup.sh kvm-setup.sh /root/
 ---> 39d5b0df245a
Step 11/11 : RUN /root/basic-setup.sh && /root/kvm-setup.sh
 ---> Running in c67ebf8063aa
Removed /etc/systemd/system/multi-user.target.wants/sshd.service.
	package libibverbs-35.0-1.fc34.x86_64 is already installed
	package librdmacm-35.0-1.fc34.x86_64 is already installed
	package libibumad-35.0-1.fc34.x86_64 is already installed
	package infiniband-diags-35.0-1.fc34.x86_64 is already installed
	package rdma-core-35.0-1.fc34.x86_64 is already installed
The command '/bin/sh -c /root/basic-setup.sh && /root/kvm-setup.sh' returned a non-zero code: 16
Traceback (most recent call last):
  File "/images/artemp/mkt/./mkt", line 25, in <module>
    utils.cmdline.main(cmd_modules, plugins)
  File "/images/artemp/mkt/utils/cmdline.py", line 147, in main
    args.func(args)
  File "/images/artemp/mkt/plugins/cmd_images.py", line 312, in cmd_images
    docker_call(cmd + ["-t", image, "-f", dockerfn, "."])
  File "/images/artemp/mkt/utils/docker.py", line 20, in docker_call
    return subprocess.check_call([
  File "/usr/lib64/python3.9/subprocess.py", line 373, in check_call
    raise CalledProcessError(retcode, cmd)
subprocess.CalledProcessError: Command '['sudo', 'docker', 'build', '-t', 'harbor.mellanox.com/mkt/kvm:fc34', '-f', 'kvm.Dockerfile', '.']' returned non-zero exit status 16.

As a solution, use v36 version in support script.

Fixes: e355a573d17a ("docker: Upgrade all tools and OS")
Reported-by: Artem Polyakov <artemp@nvidia.com>
Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

docker/fc34/support-rdma_core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ---
 # git_url: https://github.com/linux-rdma/rdma-core.git
-# git_commit: e29a698e99028e9a092bb00c03ee4bfa31ae0cf3
+# git_commit: 559083a154af62e02815969c337e90ef4c3cd173
 
 rpmbuild --build-in-place -bb redhat/rdma-core.spec --with pyverbs