Mellanox/mkt

commit b81c9a91db8da748f8556a1212203e7392705c8c

run: Open SSH connection for already present PCI container
The need to bind real devices while executing new docker container
causes to difference in "mkt run" behavior between real devices and
virtualized ones.

Consecutive runs of "mkt run" for virtualized devices create new
container every time, while "mkt run" over real devices throws
an error to indicate that device is busy.

In this change, we implemented scheme that "mkt run" over real devices
will open ssh connection to that docker instead of attempting to run
container.

Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

plugins/cmd_run.py
@@ -149,6 +149,9 @@ def get_container_name(vm_addr):
     """Return the name of the docker container to use for this VM"""
     return "mkt_run_%s" % (vm_addr.hostname)
 
+def get_host_name(cname):
+    """Return the host name of the docker container"""
+    return cname[8:]
 
 def random_mac():
     random.seed()
@@ -410,31 +413,40 @@ def cmd_run(args):
     src_dir = os.path.dirname(
         os.path.abspath(inspect.getfile(inspect.currentframe())))
 
-    cname = get_container_name(vm_addr)
-    # Clean up a container if it is left over somehow
-    cont = docker_get_containers(name=cname)
-    if cont:
-        try:
-            docker_output(["kill", *cont])
-        except subprocess.CalledProcessError:
-            pass
-        try:
-            docker_output(["rm", *cont])
-        except subprocess.CalledProcessError:
-            pass
+    ssh = False
+    if args.pci:
+        # chack if we have container running with bound PCI device to it
+        # sudo docker ps --filter "label=pci" --format "{{.Names}}"
+        # sudo docker inspect --format='{{.Config.Labels.pci}}' mkt_run_nps-server-14-015
+        cont = docker_get_containers(label="pci")
+        for c in cont:
+            c = c.decode()[1:-1]
+            cpci = docker_output(["inspect", "--format", '"{{.Config.Labels.pci}}"', c])
+            cpci = cpci.decode()[2:-2].split(', ')
+            cpci = [x[1:-1] for x in cpci]
+            common = set(cpci).intersection(set(args.pci))
+            if common:
+                ssh = True;
+                cname = c;
 
-    docker_exec(["run"] + mapdirs.as_docker_bind() + [
-        "-v",
-        "%s:/plugins:ro" % (src_dir),
-        "--rm",
-        "--net=host",
-        "--privileged",
-        "--name=%s" % (cname),
-        "--tty",
-        "--hostname",
-        vm_addr.hostname,
-        "-e",
-        "KVM_PICKLE=%s" % (get_pickle(args, vm_addr)),
-        "--interactive",
-        make_image_name("kvm", docker_os),
-    ] + do_kvm_args)
+    if ssh:
+        subprocess.call(["ssh", "root@%s" % (get_host_name(cname))])
+    else:
+        cname = get_container_name(vm_addr)
+        docker_exec(["run"] + mapdirs.as_docker_bind() + [
+            "-v",
+            "%s:/plugins:ro" % (src_dir),
+            "--rm",
+            "--net=host",
+            "--privileged",
+            "--name=%s" % (cname),
+            "--tty",
+            "-l",
+            "pci=%s" % (args.pci),
+            "--hostname",
+            vm_addr.hostname,
+            "-e",
+            "KVM_PICKLE=%s" % (get_pickle(args, vm_addr)),
+            "--interactive",
+            make_image_name("kvm", docker_os),
+        ] + do_kvm_args)

utils/docker.py
@@ -43,8 +43,8 @@ def docker_output(args, mode=None):
         raise ValueError("Bad mode %r" % (mode))
 
 
-def docker_get_containers(name):
+def docker_get_containers(label):
     containers = docker_output(
-        ["ps", "-a", "-q", "--filter",
-         "name=%s" % (name)], mode="lines")
+        ["ps", "--format", '"{{.Names}}"', "--filter",
+         "label=%s" % (label)], mode="lines")
     return containers

commit 3899a77c0f83c893e30f5a184e267b4fbc5be4e9

run: Open SSH connection or start container in transparent mode
Reimplement feature written in Fixes line, so the invocations of mkt run
will sense automatically if new container is needed to be spawned or ssh
connection should be established.

Fixes: b81c9a91db8d ("run: Open SSH connection for already present PCI container")
Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

plugins/cmd_run.py
@@ -147,9 +147,9 @@ def get_pci_rdma_devices():
     return devices
 
 
-def get_container_name(vm_addr):
+def get_container_name(args, vm_addr):
     """Return the name of the docker container to use for this VM"""
-    return "mkt_run_%s" % (vm_addr.hostname)
+    return "mkt_run_%s_%s" % (args.image, vm_addr.hostname)
 
 def get_host_name(cname):
     """Return the host name of the docker container"""
@@ -194,7 +194,7 @@ def get_mac(args):
         # docker is used to lock the MAC addresses, other virt systems should use
         # numbers at the end of the sorted range to try to avoid conflicting here.
         vm_addr = VM_Addr(ip=inf[0], mac=inf[1], hostname=host)
-        cname = get_container_name(vm_addr)
+        cname = get_container_name(args, vm_addr)
         status = docker_output([
             "ps", "-a", "-q", "--filter",
             "name=%s" % (cname), "--format", "{{.Status}}"
@@ -219,6 +219,13 @@ def configure_firewall(vm_addr):
                         stdout=subprocess.DEVNULL,
                         stderr=subprocess.DEVNULL)
 
+def get_free_port(host='127.0.0.1'):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, 0))
+    port = sock.getsockname()[1]
+    sock.close()
+
+    return port
 
 def get_pickle(args, vm_addr):
     usr = pwd.getpwuid(os.getuid())
@@ -280,6 +287,8 @@ def get_pickle(args, vm_addr):
 
     if args.gdbserver:
         p["gdbserver"] = args.gdbserver
+
+    p["port"] = args.port
 
     return base64.b64encode(pickle.dumps(p)).decode()
 
@@ -440,16 +449,25 @@ def try_to_ssh(args):
     # chack if we have container running with bound PCI device to it
     # sudo docker ps --filter "label=pci" --format "{{.Names}}"
     # sudo docker inspect --format='{{.Config.Hostname}}' mkt_run_nps-server-14-015
-    cont = docker_get_containers(label="pci")
+    cont = docker_get_containers(label="image")
     for c in cont:
         c = c.decode()[1:-1]
+        cimage = docker_output(["inspect", "--format", '"{{.Config.Labels.image}}"', c])
+        cimage = cimage.decode()[1:-1]
+        if args.image != cimage:
+            continue
+
+        cport = docker_output(["inspect", "--format", '"{{.Config.Labels.port}}"', c])
+        cport = cport.decode()[1:-1]
+
         cpci = docker_output(["inspect", "--format", '"{{.Config.Hostname}}"', c])
         if cpci:
             cname = c;
             if have_netdev("br0"):
                 cmd = ["ssh", "%s@%s" % (usr.pw_name, get_host_name(cname))]
             else:
-                cmd = ["ssh", "-p", "4444", "%s@localhost" % (usr.pw_name)]
+                cmd = ["ssh", "-o", "LogLevel=error", "-o", "StrictHostKeyChecking=no",
+                        "-p", "%s" % (cport), "%s@localhost" % (usr.pw_name)]
             subprocess.call(cmd)
             return True
 
@@ -532,13 +550,13 @@ def cmd_run(args):
     """Run a system image container inside KVM"""
 
     args.inside_mkt = is_inside_mkt()
-    if (try_to_ssh(args)):
-        return;
 
     from . import cmd_images
     section = utils.load_config_file()
-
     find_image(args, section)
+    if (try_to_ssh(args)):
+        return;
+
     setup_devices(args)
     do_bind_pci(args)
     set_kernel(args, section)
@@ -547,6 +565,7 @@ def cmd_run(args):
 
     mapdirs = build_dirlist(args, section)
     vm_addr = get_mac(args)
+    args.port = get_free_port()
 
     if not args.run_shell and not args.inside_mkt:
         # Open ports so docker instance will be able
@@ -568,7 +587,7 @@ def cmd_run(args):
         os.path.abspath(inspect.getfile(inspect.currentframe())))
 
     docker_os = section.get('os', cmd_images.default_os)
-    cname = get_container_name(vm_addr)
+    cname = get_container_name(args, vm_addr)
     docker_exec(["run"] + mapdirs.as_docker_bind() + [
         "-v",
        "%s:/plugins:ro,Z" % (src_dir),
@@ -580,7 +599,9 @@ def cmd_run(args):
        "--name=%s" % (cname),
        "--tty",
        "-l",
-       "pci=%s" % (args.pci),
+       "image=%s" % (args.image),
+       "-l",
+       "port=%s" % (args.port),
        "--hostname",
        vm_addr.hostname,
        "-e",

plugins/do-kvm.py
@@ -342,14 +342,12 @@ def set_bridge_network(args):
 
 
 def set_loop_network(args):
-    """Simple network that forwards our localhost port 4444 to the kvm's ssh
-    port"""
+    """Simple network that forwards our localhost port to the kvm's ssh port"""
     print(
-        "No br0 bridge found, using NAT networking, connected to port localhost:4444 for ssh"
-    )
+        "No br0 bridge found, using NAT networking, connected to port localhost:%s for ssh" % (args.port))
     qemu_args["-net"].extend([
         "nic,model=virtio,macaddr=%s" % (args.vm_addr.mac),
-        "user,hostfwd=tcp:127.0.0.1:4444-:22"
+        "user,hostfwd=tcp:127.0.0.1:%s-:22" % (args.port)
     ])
 
     qemu_args["-append"] += " systemd.hostname=%s" %(args.vm_addr.hostname);
@@ -542,6 +540,7 @@ def setup_from_pickle(args, pickle_params):
         with open('/etc/mkt.conf', "w") as f:
             f.write(' '.join(args.nested_pci) + '\n')
     args.inside_mkt = p.get("inside_mkt", False);
+    args.port = p.get("port", 4444);
 
 parser = argparse.ArgumentParser(
     description='Launch kvm using the filesystem from the container')