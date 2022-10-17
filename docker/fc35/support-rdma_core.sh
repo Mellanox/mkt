ff71ff54a147c8bf09f7269e6f985938613f49e1
Jason Gunthorpe <jgg@nvidia.com>
2022-10-17 14:05:23 -0300

    rdma-core: Update to v42
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- docker/fc35/support-rdma_core.sh
+++ docker/fc35/support-rdma_core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ---
 # git_url: https://github.com/linux-rdma/rdma-core.git
-# git_commit: ab6b2ecad72140ef2e82a120bbd8e00b913fdde8
+# git_commit: 196bad56ed060612e22674b668b5ec3d8659ade3
 
 rpmbuild --build-in-place -bb redhat/rdma-core.spec --with pyverbs
