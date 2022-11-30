2ea4f3ce32c9d8e2c4466dd1b43aafbcfabf1f44
Leon Romanovsky <leonro@nvidia.com>
2022-11-30 12:49:03 +0200

    rdma-core: Update to v43
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- docker/fc35/support-rdma_core.sh
+++ docker/fc35/support-rdma_core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ---
 # git_url: https://github.com/linux-rdma/rdma-core.git
-# git_commit: ab6b2ecad72140ef2e82a120bbd8e00b913fdde8
+# git_commit: f58a9b64ae717493979326514e90608b796c7ebd
 
 rpmbuild --build-in-place -bb redhat/rdma-core.spec --with pyverbs
